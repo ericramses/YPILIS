032244d152bf546b3295289939c076022f7c6f00
SidHarder <sid.harder@ypii.com>
2015-08-18 07:27:26 -0600

    Odds and ends

---
+++ Business/Test.Model/Gout.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YellowstonePathology.Business.Test.Model
+{
+	public class Gout : CytochemicalForMicroorganisms
+	{
+        public Gout()
+		{
+			this.m_TestId = 358;
+			this.m_TestName = "Gout";
+            this.m_TestAbbreviation = "Gout";
+			this.m_Active = true;
+			this.m_NeedsAcknowledgement = true;
+		}
+	}
+}
