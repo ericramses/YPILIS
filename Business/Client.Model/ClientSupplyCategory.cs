5c45433908a4e0aa9711a550f5e185347c4cd8bc
William Copland <William.Copland@ypii.com>
2015-09-02 11:11:24 -0600

    test

---
+++ Business/Client.Model/ClientSupplyCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YellowstonePathology.Business.Client.Model
+{
+	public class ClientSupplyCategory
+	{
+		public static string Cytology = "Cytology";
+		public static string Forms = "Forms";
+		public static string Histology = "Histology";
+		public static string Transport = "Transport";
+	}
+}
---
+++ UI/Client/ClientSupplyOrderDialog.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace YellowstonePathology.UI.Client
+{
+	/// <summary>
+	/// Interaction logic for ClientSupplyOrderDialog.xaml
+	/// </summary>
+	public partial class ClientSupplyOrderDialog : Window
+	{
+		public ClientSupplyOrderDialog()
+		{
+			InitializeComponent();
+		}
+	}
+}
