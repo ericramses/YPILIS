20d60b77ccf05eb8c5923ade45b3a0448f068c64
SidHarder <sid.harder@ypii.com>
2015-10-22 13:20:57 -0600

    misc

---
+++ YellowstonePathology.OptimusPrime/NGCTResult.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YellowstonePathology.OptimusPrime
+{
+    public class NGCTResult
+    {        
+        public static string PositiveResult = "Positive";
+        public static string NegativeResult = "Negative";
+        public static string InvalidResult = "Invalid";
+
+        protected string m_NGResultCode;
+        protected string m_NGResult;
+        protected string m_CTResultCode;
+        protected string m_CTResult;
+
+        public NGCTResult()
+        {
+
+        } 
+        
+        public string NGResultCode
+        {
+            get { return this.m_NGResultCode; }
+        }
+
+        public string NGResult
+        {
+            get { return this.m_NGResult; }
+        }
+
+        public string CTResultCode
+        {
+            get { return this.m_CTResultCode; }
+        }
+
+        public string CTResult
+        {
+            get { return this.m_CTResult; }
+        }
+    }
+}
---
+++ YellowstonePathology.OptimusPrime/NGCTResultHandler.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace YellowstonePathology.OptimusPrime
+{
+    public class NGCTResultHandler
+    {
+        public async Task<object> Invoke(object input)
+        {
+            var payload = (IDictionary<string, object>)input;
+            return await HandleResult(payload);
+        }
+
+        public async Task<string> HandleResult(IDictionary<string, object> payload)
+        {
+            var connectionString = "Data Source=TestSQL;Initial Catalog=YPIData;Integrated Security=True";
+            
+            string testName = (string)payload["testName"];
+            string aliquotOrderId = (string)payload["aliquotOrderId"];
+            string overallInterpretation = (string)payload["overallInterpretation"];
+            string sql = null;
+
+            if (testName == "NGCT")
+            {
+                /*
+                NGCTResult ngctResult = null;
+                if (overallInterpretation == "Negative")
+                {                    
+                    ngctResult = new HPVResult();
+                    sql = @"Update tblPanelSetOrderHPVTWI set Result = '" + hpvResult.Result + "' "
+                        + "from tblPanelSetOrderHPVTWI psoh, tblPanelSetOrder pso "
+                        + "where psoh.ReportNo = pso.ReportNo "
+                        + "and pso.OrderedOnId = '" + aliquotOrderId + "' and pso.Accepted = 0; ";                        
+
+                    sql += @"Update tblPanelSetOrder set ResultCode = '" + hpvResult.ResultCode + "', "
+                    + "[HoldDistribution] = 0, "
+                    + "[Accepted] = 1, "
+                    + "[AcceptedBy] = 'Optimus Prime', "
+                    + "[AcceptedById] = 5134, "
+                    + "[AcceptedDate] = '" + DateTime.Today.ToString("MM/dd/yyyy") + "', "
+                    + "[AcceptedTime] = '" + DateTime.Now.ToString("MM/dd/yyyy HH:mm") + "', "
+                    + "[Final] = 1, "
+                    + "[Signature] = 'Optimus Prime', "
+                    + "[FinaledById] = 5134, "
+                    + "[FinalDate] = '" + DateTime.Today.ToString("MM/dd/yyyy") + "', "
+                    + "[FinalTime] = '" + DateTime.Now.ToString("MM/dd/yyyy HH:mm") + "' "
+                    + "where PanelSetId = 14 and Accepted = 0 and OrderedOnId = '" + aliquotOrderId + "';";                    
+                }
+                else if (overallInterpretation == "POSITIVE")
+                {
+                    hpvResult = new HPVPositiveResult();
+                    sql = @"Update tblPanelSetOrderHPVTWI set Result = '" + hpvResult.Result + "' "
+                        + "from tblPanelSetOrderHPVTWI psoh, tblPanelSetOrder pso "
+                        + "where psoh.ReportNo = pso.ReportNo "
+                        + "and pso.OrderedOnId = '" + aliquotOrderId + "' and pso.Accepted = 0; ";                        
+
+                    sql += @"Update tblPanelSetOrder set ResultCode = '" + hpvResult.ResultCode + "', "
+                    + "[HoldDistribution] = 0, "
+                    + "[Accepted] = 1, "
+                    + "[AcceptedBy] = 'Optimus Prime', "
+                    + "[AcceptedById] = 5134, "
+                    + "[AcceptedDate] = '" + DateTime.Today.ToString("MM/dd/yyyy") + "', "
+                    + "[AcceptedTime] = '" + DateTime.Now.ToString("MM/dd/yyyy HH:mm") + "' "
+                    + "where PanelSetId = 14 and Accepted = 0 and OrderedOnId = '" + aliquotOrderId + "';";
+                }
+                else if (overallInterpretation == "Invalid")
+                {
+                    hpvResult = new HPVInvalidResult();
+                    sql = @"Update tblPanelSetOrderHPVTWI set Result = '" + hpvResult.Result + "' "
+                        + "from tblPanelSetOrderHPVTWI psoh, tblPanelSetOrder pso "
+                        + "where psoh.ReportNo = pso.ReportNo "
+                        + "and pso.OrderedOnId = '" + aliquotOrderId + "' and pso.Accepted = 0; ";                    
+
+                    sql += @"Update tblPanelSetOrder set ResultCode = '" + hpvResult.ResultCode + "', "
+                    + "[HoldDistribution] = 0 "                    
+                    + "where PanelSetId = 14 and Accepted = 0 and OrderedOnId = '" + aliquotOrderId + "';";                    
+                }
+            }
+            */
+
+            using (var cnx = new SqlConnection(connectionString))
+            {
+                using (var cmd = new SqlCommand(sql, cnx))
+                {
+                    await cnx.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+
+            return "Optimus Prime updated result: " + aliquotOrderId + " - " + testName;
+        }
+    }
+}
