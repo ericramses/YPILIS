acd54d462c862b0df760a5e0228d62c9299c0774
WilliamCopland <william.copland@ypii.com>
2015-12-10 10:09:24 -0700

    Issue217 2

---
+++ Business/Test/PDL1/PDL1NegativeResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YellowstonePathology.Business.Test.PDL1
+{
+    public class PDL1NegativeResult : TestResult
+    {
+        public PDL1NegativeResult()
+        {
+            this.m_Result = "Negative";
+            this.m_ResultCode = "PDL1NGTV";
+        }
+    }
+}
