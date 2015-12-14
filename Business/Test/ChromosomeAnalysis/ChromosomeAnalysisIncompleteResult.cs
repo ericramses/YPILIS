503d531bb2bcc9dc82976d4725e319b255e7dee2
SidHarder <softwarenavigator@gmail.com>
2015-12-14 13:22:15 -0700

    Add the incomplete result item

---
+++ Business/Test/ChromosomeAnalysis/ChromosomeAnalysisIncompleteResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YellowstonePathology.Business.Test.ChromosomeAnalysis
+{
+	public class ChromosomeAnalysisIncompleteResult : YellowstonePathology.Business.Test.TestResult
+	{
+        public ChromosomeAnalysisIncompleteResult()
+		{
+			this.m_Result = "Incomplete";
+			this.m_ResultCode = "CHRMSMNLYSNCMPLT";
+		}
+	}
+}
