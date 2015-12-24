ed2ea2a813e421b928eef4e27773550c265fb227
SidHarder <softwarenavigator@gmail.com>
2015-12-24 12:37:18 -0700

    specimen label printer

---
+++ Business/Label.Model/SpecimenLabel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YellowstonePathology.Business.Label.Model
+{
+    public class SpecimenLabel
+    {        
+        protected string m_AliquotOrderId;
+        protected string m_MasterAccessionNo;             
+        protected string m_PatientInitials;
+        protected string m_AliquotLabel;                        
+                
+        public SpecimenLabel()
+        {
+
+        }        
+
+        public void FromAliquotOrder(string aliquotOrderId, string aliquotLabel, string masterAccessionNo, string pLastName, string pFirstName)
+        {
+            this.m_AliquotOrderId = aliquotOrderId;
+            this.m_MasterAccessionNo = masterAccessionNo;
+            YellowstonePathology.Business.PatientName patientName = new PatientName(pLastName, pFirstName);
+            this.m_PatientInitials = patientName.GetInitials();
+            this.m_AliquotLabel = aliquotLabel;
+        }
+
+        public virtual void DrawLabel(int x, int y, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            DataMatrix.DmtxImageEncoder encoder = new DataMatrix.DmtxImageEncoder();
+            DataMatrix.DmtxImageEncoderOptions options = new DataMatrix.DmtxImageEncoderOptions();
+            options.ModuleSize = 2;
+            options.MarginSize = 3;
+            options.BackColor = System.Drawing.Color.White;
+            options.ForeColor = System.Drawing.Color.Black;
+
+            string barcodeId = YellowstonePathology.Business.BarcodeScanning.BarcodePrefixEnum.HBLK + this.m_AliquotOrderId;
+            System.Drawing.Bitmap barcodeBitmap = encoder.EncodeImage(barcodeId, options);
+
+            e.Graphics.DrawString(this.m_MasterAccessionNo, new System.Drawing.Font("Verdana", 9), System.Drawing.Brushes.Black, new System.Drawing.PointF(x + 2, y));                        
+            e.Graphics.DrawImage(barcodeBitmap, new System.Drawing.Point(x + 2, y + 18));
+            
+            e.Graphics.DrawString(this.m_PatientInitials, new System.Drawing.Font("Verdana", 6), System.Drawing.Brushes.Black, new System.Drawing.PointF(x + 2, y + 60));
+            //e.Graphics.DrawString(this.m_AliquotLabel, new System.Drawing.Font("Verdana", 6), System.Drawing.Brushes.Black, new System.Drawing.PointF(x + 50, y + 60));            
+        }
+    }
+}
---
+++ Business/Label.Model/SpecimenLabelPrinter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YellowstonePathology.Business.Label.Model
+{
+    public class SpecimenLabelPrinter
+    {
+        private System.Drawing.Printing.PrintDocument m_PrintDocument;        
+        private Queue<SpecimenLabel> m_SpecimenLabelQueue;        
+
+        public SpecimenLabelPrinter(YellowstonePathology.Business.Test.AliquotOrderCollection aliquotOrderCollection, YellowstonePathology.Business.Test.AccessionOrder accessionOrder)
+        {            
+            this.m_SpecimenLabelQueue = new Queue<SpecimenLabel>();
+            foreach (YellowstonePathology.Business.Test.AliquotOrder aliquotOrder in aliquotOrderCollection)
+            {
+                if (aliquotOrder.IsSpecimen() == true)
+                {                                        
+                    SpecimenLabel specimenLabel = new SpecimenLabel();
+                    specimenLabel.FromAliquotOrder(aliquotOrder.AliquotOrderId, aliquotOrder.Label, accessionOrder.MasterAccessionNo, accessionOrder.PLastName, accessionOrder.PFirstName);
+                    this.m_SpecimenLabelQueue.Enqueue(specimenLabel);
+                    aliquotOrder.Printed = true;                    
+                }
+            }       
+        }
+
+        public SpecimenLabelPrinter(string aliquotOrderId, string aliquotLabel, string masterAccessionNo, string pLastName, string pFirstName)
+        {
+            this.m_SpecimenLabelQueue = new Queue<SpecimenLabel>();
+            SpecimenLabel specimenLabel = new SpecimenLabel();
+            specimenLabel.FromAliquotOrder(aliquotOrderId, aliquotLabel, masterAccessionNo, pLastName, pFirstName);
+            this.m_SpecimenLabelQueue.Enqueue(specimenLabel);        
+        }        
+
+        public bool HasItemsToPrint()
+        {
+            bool result = false;
+            if (this.m_SpecimenLabelQueue.Count > 0) result = true;
+            return result;
+        }        
+
+        public void Print()
+        {
+            this.m_PrintDocument = new System.Drawing.Printing.PrintDocument();
+            System.Printing.PrintServer printServer = new System.Printing.LocalPrintServer();
+            System.Printing.PrintQueue printQueue = printServer.GetPrintQueue(YellowstonePathology.Business.User.UserPreferenceInstance.Instance.UserPreference.HistologySlideLabelPrinter);
+            this.m_PrintDocument.PrinterSettings.PrinterName = printQueue.FullName;
+            this.m_PrintDocument.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(PrintDocument_PrintPage);
+            this.m_PrintDocument.Print();            
+        }
+
+        private void PrintDocument_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            int count = 1;
+            int x = 3;
+            int y = 8;
+
+            for (int i = 0; i < 4; i++)
+            {
+                SpecimenLabel specimenLabel = this.m_SpecimenLabelQueue.Dequeue();
+                specimenLabel.DrawLabel(x, y, e);                
+                x = x + 106;
+                count += 1;
+                if (this.m_SpecimenLabelQueue.Count == 0) break;
+            }
+
+            if (this.m_SpecimenLabelQueue.Count == 0)
+            {
+                e.HasMorePages = false;
+            }
+            else
+            {
+                e.HasMorePages = true;
+            }
+        }  
+    }
+}
