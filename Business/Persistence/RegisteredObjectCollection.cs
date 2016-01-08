699501c5f1c0cea330d9422ed3693ed8ebd68c48
WilliamCopland <william.copland@ypii.com>
2016-01-08 13:01:45 -0700

    Add Registered Object and Registered Object Collection

---
+++ Business/Persistence/RegisteredObjectCollection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace YellowstonePathology.Business.Persistence
+{
+    public class RegisteredObjectCollection : ObservableCollection<RegisteredObject>
+    {
+        public RegisteredObjectCollection()
+        { }
+
+        public RegisteredObject Get(object key)
+        {
+            RegisteredObject result = null;
+            foreach (RegisteredObject registeredObject in this)
+            {
+                if(registeredObject.Key == key)
+                {
+                    result = registeredObject;
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        public bool Exists(object key)
+        {
+            bool result = false;
+            foreach (RegisteredObject registeredObject in this)
+            {
+                if (registeredObject.Key == key)
+                {
+                    result = true;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
