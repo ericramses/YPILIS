642c8695a91d0d76e8b95291ad89d9b40734ef08
SidHarder <softwarenavigator@gmail.com>
2016-01-21 11:03:41 -0700

    hpv1618 method

--- Business/Test/HPV1618/HPV1618Result.cs
+++ Business/Test/HPV1618/HPV1618Result.cs
@@ -12,7 +12,13 @@ namespace YellowstonePathology.Business.Test.HPV1618
         public static string InvalidResult = "Invalid";
         public static string InvalidResultCode = "HPV1618NVLD";
 
-        public static string Method = null;
+        public static string Method = "The Aptima HPV 16 18/45 genotype assay is an in vitro nucleic acid amplification test for the " +
+            "qualitative detection of E6/E7 viral messenger RNA(mRNA) of human papillomavirus(HPV) " +
+            "types 16, 18, and 45 in cervical specimens from women with Aptima HPV assay positive results. " +
+            "The Aptima HPV 16 18/45 genotype assay can differentiate HPV 16 from HPV 18 and/or HPV 45, " +
+            "but does not differentiate between HPV 18 and HPV 45. Cervical specimens in ThinPrep Pap " +
+            "Test vials containing PreservCyt Solution and collected with broom-type or cytobrush/spatula " +
+            "collection devices* may be tested with the Aptima HPV 16 18/45 genotype assay. ";
 
         public static string References = "Khan, M.J., P.E. Castle, A.T. Lorincz, S. Wacholder, M. Sherman, D.R. Scott, B.B. Rush, A.G. Glass and M. Schiffman. 2005. " +
             "The elevated 10-year risk of cervical precancer and cancer in women with human papillomavirus (HPV) type 16 or 18 and the possible utility of type-specific HPV testing in " +
