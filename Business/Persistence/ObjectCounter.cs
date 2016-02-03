18f1d37545029f7fa7e129976a76b96eee337d13
WilliamCopland <william.copland@ypii.com>
2016-02-03 09:57:03 -0700

    Add ObjectCounter and Collection

---
+++ Business/Persistence/ObjectCounter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YellowstonePathology.Business.Persistence
+{
+    public class ObjectCounter
+    {
+        private Type m_ObjectType;
+        private int m_ObjectCount;
+
+        public ObjectCounter(Type objectType)
+        {
+            this.m_ObjectType = objectType;
+            this.m_ObjectCount = 1;
+        }
+
+        public Type ObjectType
+        {
+            get { return this.m_ObjectType; }
+        }
+
+        public int ObjectCount
+        {
+            get { return this.m_ObjectCount; }
+        }
+    }
+}
---
+++ Business/Persistence/ObjectCounterCollection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace YellowstonePathology.Business.Persistence
+{
+    public class ObjectCounterCollection : ObservableCollection<ObjectCounter>
+    {
+        public ObjectCounterCollection()
+        { }
+    }
+}
