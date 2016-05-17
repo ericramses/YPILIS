17e459463a8e6c5697bb3590e2ee8d16104b2ee7
WilliamCopland <william.copland@ypii.com>
2016-05-17 10:34:52 -0600

    Move HER2ByISH, TCELLSubsetAnalysis to ResultControl

--- UI/Test/TCellSubsetAnalysisResultPage.xaml.cs
+++ UI/Test/TCellSubsetAnalysisResultPage.xaml.cs
@@ -22,7 +22,7 @@ namespace YellowstonePathology.UI.Test
 	/// <summary>
 	/// Interaction logic for TCellSubsetAnalysisResultPage.xaml
 	/// </summary>
-	public partial class TCellSubsetAnalysisResultPage : UserControl, INotifyPropertyChanged
+	public partial class TCellSubsetAnalysisResultPage : ResultControl, INotifyPropertyChanged
 	{
 		public event PropertyChangedEventHandler PropertyChanged;
 
@@ -36,7 +36,7 @@ namespace YellowstonePathology.UI.Test
 		
 		public TCellSubsetAnalysisResultPage(YellowstonePathology.Business.Test.TCellSubsetAnalysis.TCellSubsetAnalysisTestOrder testOrder,
 			YellowstonePathology.Business.Test.AccessionOrder accessionOrder,
-			YellowstonePathology.Business.User.SystemIdentity systemIdentity)
+			YellowstonePathology.Business.User.SystemIdentity systemIdentity) : base(testOrder, accessionOrder)
 		{
 			this.m_PanelSetOrder = testOrder;
 			this.m_AccessionOrder = accessionOrder;
@@ -46,6 +46,10 @@ namespace YellowstonePathology.UI.Test
 			InitializeComponent();
 
 			DataContext = this;
+
+            this.m_ControlsNotDisabledOnFinal.Add(this.ButtonNext);
+            this.m_ControlsNotDisabledOnFinal.Add(this.TextBlockShowDocument);
+            this.m_ControlsNotDisabledOnFinal.Add(this.TextBlockUnfinalResults);
         }
 
         public YellowstonePathology.Business.Test.TCellSubsetAnalysis.TCellSubsetAnalysisTestOrder PanelSetOrder
