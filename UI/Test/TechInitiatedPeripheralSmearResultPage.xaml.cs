cad276a2a0ef8b9fb03000350973458057530d51
WilliamCopland <william.copland@ypii.com>
2016-05-18 12:31:23 -0600

    TechInitiatedPerifSmear to Result Control

--- UI/Test/TechInitiatedPeripheralSmearResultPage.xaml.cs
+++ UI/Test/TechInitiatedPeripheralSmearResultPage.xaml.cs
@@ -18,7 +18,7 @@ namespace YellowstonePathology.UI.Test
     /// <summary>
     /// Interaction logic for TechInitiatedPeripheralSmearResultPage.xaml
     /// </summary>
-    public partial class TechInitiatedPeripheralSmearResultPage : UserControl, INotifyPropertyChanged 
+    public partial class TechInitiatedPeripheralSmearResultPage : ResultControl, INotifyPropertyChanged 
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -33,7 +33,7 @@ namespace YellowstonePathology.UI.Test
 
         public TechInitiatedPeripheralSmearResultPage(YellowstonePathology.Business.Test.TechInitiatedPeripheralSmear.TechInitiatedPeripheralSmearTestOrder testOrder,
             YellowstonePathology.Business.Test.AccessionOrder accessionOrder,
-            YellowstonePathology.Business.User.SystemIdentity systemIdentity)
+            YellowstonePathology.Business.User.SystemIdentity systemIdentity) : base(testOrder, accessionOrder)
         {
             this.m_PanelSetOrder = testOrder;
             this.m_AccessionOrder = accessionOrder;
@@ -44,6 +44,10 @@ namespace YellowstonePathology.UI.Test
             InitializeComponent();
 
             DataContext = this;
+
+            this.m_ControlsNotDisabledOnFinal.Add(this.ButtonNext);
+            this.m_ControlsNotDisabledOnFinal.Add(this.TextBlockShowDocument);
+            this.m_ControlsNotDisabledOnFinal.Add(this.TextBlockUnfinalResults);
         }
 
         public YellowstonePathology.Business.Test.TechInitiatedPeripheralSmear.TechInitiatedPeripheralSmearTestOrder PanelSetOrder
