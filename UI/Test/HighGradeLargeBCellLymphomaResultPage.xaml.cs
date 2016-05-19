13806f90f32e32c0a0f69c0e0ca5d3148c91a7bc
WilliamCopland <william.copland@ypii.com>
2016-05-19 12:12:29 -0600

    HighGradeLargeBCellLymphoma to Result Control

--- UI/Test/HighGradeLargeBCellLymphomaResultPage.xaml.cs
+++ UI/Test/HighGradeLargeBCellLymphomaResultPage.xaml.cs
@@ -18,7 +18,7 @@ namespace YellowstonePathology.UI.Test
 	/// <summary>
 	/// Interaction logic for HighGradeLargeBCellLymphomaResultPage.xaml
 	/// </summary>
-	public partial class HighGradeLargeBCellLymphomaResultPage : UserControl, INotifyPropertyChanged 
+	public partial class HighGradeLargeBCellLymphomaResultPage : ResultControl, INotifyPropertyChanged 
 	{
 		public event PropertyChangedEventHandler PropertyChanged;
 
@@ -34,7 +34,7 @@ namespace YellowstonePathology.UI.Test
 
 		public HighGradeLargeBCellLymphomaResultPage(YellowstonePathology.Business.Test.HighGradeLargeBCellLymphoma.PanelSetOrderHighGradeLargeBCellLymphoma panelSetOrderHighGradeLargeBCellLymphoma,
 			YellowstonePathology.Business.Test.AccessionOrder accessionOrder,
-			YellowstonePathology.Business.User.SystemIdentity systemIdentity)
+			YellowstonePathology.Business.User.SystemIdentity systemIdentity) : base(panelSetOrderHighGradeLargeBCellLymphoma, accessionOrder)
 		{
 			this.m_PanelSetOrder = panelSetOrderHighGradeLargeBCellLymphoma;
 			this.m_AccessionOrder = accessionOrder;
@@ -48,6 +48,10 @@ namespace YellowstonePathology.UI.Test
 			InitializeComponent();
 
 			DataContext = this;
+
+            this.m_ControlsNotDisabledOnFinal.Add(this.ButtonNext);
+            this.m_ControlsNotDisabledOnFinal.Add(this.TextBlockShowDocument);
+            this.m_ControlsNotDisabledOnFinal.Add(this.TextBlockUnfinalResults);
         }
 
         public string OrderedOnDescription
