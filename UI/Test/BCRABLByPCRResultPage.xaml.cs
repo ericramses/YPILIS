0a0b50d0d608d9fb5dd505d24524375b6f133520
WilliamCopland <william.copland@ypii.com>
2016-05-20 10:20:01 -0600

    BCRABLByPCR to Result Control

--- UI/Test/BCRABLByPCRResultPage.xaml.cs
+++ UI/Test/BCRABLByPCRResultPage.xaml.cs
@@ -18,7 +18,7 @@ namespace YellowstonePathology.UI.Test
 	/// <summary>
 	/// Interaction logic for BCRABLByPCRResultPage.xaml
 	/// </summary>
-	public partial class BCRABLByPCRResultPage : UserControl, INotifyPropertyChanged 
+	public partial class BCRABLByPCRResultPage : ResultControl, INotifyPropertyChanged 
 	{
 		public event PropertyChangedEventHandler PropertyChanged;
 
@@ -35,7 +35,7 @@ namespace YellowstonePathology.UI.Test
 
 		public BCRABLByPCRResultPage(YellowstonePathology.Business.Test.BCRABLByPCR.BCRABLByPCRTestOrder testOrder,
 			YellowstonePathology.Business.Test.AccessionOrder accessionOrder,
-			YellowstonePathology.Business.User.SystemIdentity systemIdentity)
+			YellowstonePathology.Business.User.SystemIdentity systemIdentity) : base(testOrder, accessionOrder)
 		{
 			this.m_PanelSetOrder = testOrder;
 			this.m_AccessionOrder = accessionOrder;
@@ -50,6 +50,10 @@ namespace YellowstonePathology.UI.Test
 			InitializeComponent();
 
 			DataContext = this;
+
+            this.m_ControlsNotDisabledOnFinal.Add(this.ButtonNext);
+            this.m_ControlsNotDisabledOnFinal.Add(this.TextBlockShowDocument);
+            this.m_ControlsNotDisabledOnFinal.Add(this.TextBlockUnfinalResults);
         }
 
         public string OrderedOnDescription
