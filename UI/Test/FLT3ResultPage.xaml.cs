370e188293078d7f6233cfef5a8e89d6df9f5059
WilliamCopland <william.copland@ypii.com>
2016-05-20 11:00:00 -0600

    FLT3 to Result Control

--- UI/Test/FLT3ResultPage.xaml.cs
+++ UI/Test/FLT3ResultPage.xaml.cs
@@ -18,7 +18,7 @@ namespace YellowstonePathology.UI.Test
 	/// <summary>
 	/// Interaction logic for FLT3ResultPage.xaml
 	/// </summary>
-	public partial class FLT3ResultPage : UserControl, INotifyPropertyChanged 
+	public partial class FLT3ResultPage : ResultControl, INotifyPropertyChanged 
 	{
 		public event PropertyChangedEventHandler PropertyChanged;
 
@@ -34,7 +34,7 @@ namespace YellowstonePathology.UI.Test
 
 		public FLT3ResultPage(YellowstonePathology.Business.Test.FLT3.PanelSetOrderFLT3 panelSetOrderFLT3,
 			YellowstonePathology.Business.Test.AccessionOrder accessionOrder,
-			YellowstonePathology.Business.User.SystemIdentity systemIdentity)
+			YellowstonePathology.Business.User.SystemIdentity systemIdentity) : base(panelSetOrderFLT3, accessionOrder)
 		{
 			this.m_PanelSetOrder = panelSetOrderFLT3;
 			this.m_AccessionOrder = accessionOrder;
@@ -48,6 +48,10 @@ namespace YellowstonePathology.UI.Test
 			InitializeComponent();
 
 			DataContext = this;
+
+            this.m_ControlsNotDisabledOnFinal.Add(this.ButtonNext);
+            this.m_ControlsNotDisabledOnFinal.Add(this.TextBlockShowDocument);
+            this.m_ControlsNotDisabledOnFinal.Add(this.TextBlockUnfinalResults);
         }
 
         public string OrderedOnDescription
