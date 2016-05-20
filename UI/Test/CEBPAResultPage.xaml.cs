e583a2f569d07da4268d04637edef8e58bd6cdd1
WilliamCopland <william.copland@ypii.com>
2016-05-20 10:55:21 -0600

    CEBPA to Result Control

--- UI/Test/CEBPAResultPage.xaml.cs
+++ UI/Test/CEBPAResultPage.xaml.cs
@@ -18,7 +18,7 @@ namespace YellowstonePathology.UI.Test
 	/// <summary>
 	/// Interaction logic for CEBPAResultPage.xaml
 	/// </summary>
-	public partial class CEBPAResultPage : UserControl, INotifyPropertyChanged 
+	public partial class CEBPAResultPage : ResultControl, INotifyPropertyChanged 
 	{
 		public event PropertyChangedEventHandler PropertyChanged;
 
@@ -34,7 +34,7 @@ namespace YellowstonePathology.UI.Test
 
 		public CEBPAResultPage(YellowstonePathology.Business.Test.CEBPA.PanelSetOrderCEBPA panelSetOrderCEBPA,
 			YellowstonePathology.Business.Test.AccessionOrder accessionOrder,
-			YellowstonePathology.Business.User.SystemIdentity systemIdentity)
+			YellowstonePathology.Business.User.SystemIdentity systemIdentity) : base(panelSetOrderCEBPA, accessionOrder)
 		{
 			this.m_PanelSetOrder = panelSetOrderCEBPA;
 			this.m_AccessionOrder = accessionOrder;
@@ -48,6 +48,10 @@ namespace YellowstonePathology.UI.Test
 			InitializeComponent();
 
 			DataContext = this;
+
+            this.m_ControlsNotDisabledOnFinal.Add(this.ButtonNext);
+            this.m_ControlsNotDisabledOnFinal.Add(this.TextBlockShowDocument);
+            this.m_ControlsNotDisabledOnFinal.Add(this.TextBlockUnfinalResults);
         }
 
         public string OrderedOnDescription
