7a0f91d44df7e4409038891cfffe720bee132c06
WilliamCopland <william.copland@ypii.com>
2016-05-23 09:46:22 -0600

    NeoARRAYSNPCytogeneticProfile and PlasmaCellMyelomaRiskStratification to Result Control

--- UI/Test/PlasmaCellMyelomaRiskStratificationResultPage.xaml.cs
+++ UI/Test/PlasmaCellMyelomaRiskStratificationResultPage.xaml.cs
@@ -18,7 +18,7 @@ namespace YellowstonePathology.UI.Test
 	/// <summary>
 	/// Interaction logic for PlasmaCellMyelomaRiskStratificationResultPage.xaml
 	/// </summary>
-	public partial class PlasmaCellMyelomaRiskStratificationResultPage : UserControl, INotifyPropertyChanged 
+	public partial class PlasmaCellMyelomaRiskStratificationResultPage : ResultControl, INotifyPropertyChanged 
 	{
 		public event PropertyChangedEventHandler PropertyChanged;
 
@@ -34,7 +34,7 @@ namespace YellowstonePathology.UI.Test
 
 		public PlasmaCellMyelomaRiskStratificationResultPage(YellowstonePathology.Business.Test.PlasmaCellMyelomaRiskStratification.PlasmaCellMyelomaRiskStratificationTestOrder plasmaCellMyelomaRiskStratificationTestOrder,
 			YellowstonePathology.Business.Test.AccessionOrder accessionOrder,
-			YellowstonePathology.Business.User.SystemIdentity systemIdentity)
+			YellowstonePathology.Business.User.SystemIdentity systemIdentity) : base(plasmaCellMyelomaRiskStratificationTestOrder, accessionOrder)
 		{
 			this.m_PanelSetOrder = plasmaCellMyelomaRiskStratificationTestOrder;
 			this.m_AccessionOrder = accessionOrder;
@@ -48,6 +48,10 @@ namespace YellowstonePathology.UI.Test
 			InitializeComponent();
 
 			DataContext = this;
+
+            this.m_ControlsNotDisabledOnFinal.Add(this.ButtonNext);
+            this.m_ControlsNotDisabledOnFinal.Add(this.TextBlockShowDocument);
+            this.m_ControlsNotDisabledOnFinal.Add(this.TextBlockUnfinalResults);
         }
 
         public string OrderedOnDescription
