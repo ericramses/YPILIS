8e9b35732ec24cf7533dcec369dbec5e151fed35
WilliamCopland <william.copland@ypii.com>
2016-06-03 12:46:32 -0600

    Add FGFR1

---
+++ YellowstonePathology/Business/Test/FGFR1/FGFR1EPICOBXView.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace YellowstonePathology.Business.Test.FGFR1
+{
+    public class FGFR1EPICOBXView : YellowstonePathology.Business.HL7View.EPIC.EPICObxView
+    {
+        public FGFR1EPICOBXView(YellowstonePathology.Business.Test.AccessionOrder accessionOrder, string reportNo, int obxCount)
+			: base(accessionOrder, reportNo, obxCount)
+		{
+        }
+
+        public override void ToXml(XElement document)
+        {
+            FGFR1TestOrder testOrder = (FGFR1TestOrder)this.m_AccessionOrder.PanelSetOrderCollection.GetPanelSetOrder(this.m_ReportNo);
+            this.AddHeader(document, testOrder, "FGFR1");
+
+            this.AddNextObxElement("", document, "F");
+            string result = "Result: " + testOrder.Result;
+            this.AddNextObxElement(result, document, "F");
+
+            this.AddNextObxElement("", document, "F");
+            this.AddNextObxElement("Comment:", document, "F");
+            this.HandleLongString(testOrder.Comment, document, "F");
+
+            this.AddNextObxElement("", document, "F");
+            this.AddNextObxElement("Pathologist: " + testOrder.Signature, document, "F");
+            if (testOrder.FinalTime.HasValue == true)
+            {
+                this.AddNextObxElement("E-signed " + testOrder.FinalTime.Value.ToString("MM/dd/yyyy HH:mm"), document, "F");
+            }
+
+            this.AddNextObxElement("", document, "F");
+            this.AddAmendments(document);
+
+            this.AddNextObxElement("Specimen Information:", document, "F");
+            YellowstonePathology.Business.Specimen.Model.SpecimenOrder specimenOrder = this.m_AccessionOrder.SpecimenOrderCollection.GetSpecimenOrder(testOrder.OrderedOn, testOrder.OrderedOnId);
+            this.AddNextObxElement("Specimen Identification: " + specimenOrder.Description, document, "F");
+            string collectionDateTimeString = YellowstonePathology.Business.Helper.DateTimeExtensions.CombineDateAndTime(specimenOrder.CollectionDate, specimenOrder.CollectionTime);
+            this.AddNextObxElement("Collection Date/Time: " + collectionDateTimeString, document, "F");
+
+            this.AddNextObxElement("", document, "F");
+            this.AddNextObxElement("Interpretation:", document, "F");
+            this.HandleLongString(testOrder.Interpretation, document, "F");
+
+            this.AddNextObxElement("", document, "F");
+            this.AddNextObxElement("Method:", document, "F");
+            this.HandleLongString(testOrder.Method, document, "F");
+
+            this.AddNextObxElement("", document, "F");
+            this.AddNextObxElement("References:", document, "F");
+            this.HandleLongString(testOrder.References, document, "F");
+
+            this.AddNextObxElement("", document, "F");
+            this.HandleLongString(testOrder.ASR, document, "F");
+
+            this.AddNextObxElement("", document, "F");
+            string locationPerformed = testOrder.GetLocationPerformedComment();
+            this.AddNextObxElement(locationPerformed, document, "F");
+            this.AddNextObxElement(string.Empty, document, "F");
+        }
+    }
+}
---
+++ YellowstonePathology/UI/Test/FGFR1ResultPath.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowstonePathology.UI.Test
+{
+    public class FGFR1ResultPath : ResultPath
+    {
+        FGFR1ResultPage m_ResultPage;
+        YellowstonePathology.Business.Test.AccessionOrder m_AccessionOrder;
+        YellowstonePathology.Business.Test.FGFR1.FGFR1TestOrder m_TestOrder;
+
+        public FGFR1ResultPath(string reportNo,
+            YellowstonePathology.Business.Test.AccessionOrder accessionOrder,
+            YellowstonePathology.UI.Navigation.PageNavigator pageNavigator,
+            System.Windows.Window window) 
+            : base(pageNavigator, window)
+        {
+            this.m_AccessionOrder = accessionOrder;
+            this.m_TestOrder = (YellowstonePathology.Business.Test.FGFR1.FGFR1TestOrder)this.m_AccessionOrder.PanelSetOrderCollection.GetPanelSetOrder(reportNo);
+        }
+
+        protected override void ShowResultPage()
+        {
+            this.m_ResultPage = new FGFR1ResultPage(this.m_TestOrder, this.m_AccessionOrder, this.m_SystemIdentity);
+            this.m_ResultPage.Next += new FGFR1ResultPage.NextEventHandler(ResultPage_Next);
+            this.m_PageNavigator.Navigate(this.m_ResultPage);
+        }
+
+        private void ResultPage_Next(object sender, EventArgs e)
+        {
+            this.Finished();
+        }
+    }
+}
