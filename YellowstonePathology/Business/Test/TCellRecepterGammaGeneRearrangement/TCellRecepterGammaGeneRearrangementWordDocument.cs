f280d0e7ea9b59314b3879734242f5cdd277c838
WilliamCopland <william.copland@ypii.com>
2016-06-13 10:02:14 -0600

    Rename template file TCell...PCR to TCellREceptor...Rearrangement and in WordDocument

--- YellowstonePathology/Business/Test/TCellRecepterGammaGeneRearrangement/TCellRecepterGammaGeneRearrangementWordDocument.cs
+++ YellowstonePathology/Business/Test/TCellRecepterGammaGeneRearrangement/TCellRecepterGammaGeneRearrangementWordDocument.cs
@@ -17,7 +17,7 @@ namespace YellowstonePathology.Business.Test.TCellRecepterGammaGeneRearrangement
 		{
             TCellRecepterGammaGeneRearrangementTestOrder tCellRecepterGammaGeneRearrangementTestOrder = (TCellRecepterGammaGeneRearrangementTestOrder)this.m_PanelSetOrder;
 
-			this.m_TemplateName = @"\\CFileServer\Documents\ReportTemplates\XmlTemplates\TCellClonalityByPCR.xml";
+			this.m_TemplateName = @"\\CFileServer\Documents\ReportTemplates\XmlTemplates\TCellRecepterGammaGeneRearrangement.xml";
 			base.OpenTemplate();
 
 			this.SetDemographicsV2();
