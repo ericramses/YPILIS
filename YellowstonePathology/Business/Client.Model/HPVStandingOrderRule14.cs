b59b928d25d8695c4e46f6471b79de5c70db2865
SidHarder <softwarenavigator@gmail.com>
2016-06-29 09:29:58 -0600

    billing

---
+++ YellowstonePathology/Business/Client.Model/HPVStandingOrderRule14.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YellowstonePathology.Business.Client.Model
+{
+    public class HPVStandingOrderRule14 : StandingOrder
+    {        
+        public HPVStandingOrderRule14()
+        {
+            this.m_StandingOrderCode = "STNDHPVRL14";            
+            this.m_ReflexOrder = new HPVReflexOrderRule14();
+            this.m_Description = this.m_ReflexOrder.Description;            
+        }        
+    }
+}
