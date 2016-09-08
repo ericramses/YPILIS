1c1053edb4f5aa512c35b215a49bf88814989a2c
SidHarder <softwarenavigator@gmail.com>
2016-09-08 11:04:16 -0600

    fedex

--- YellowstonePathology/Business/Test/ThrombocytopeniaProfile/ThrombocytopeniaProfileTest.cs
+++ YellowstonePathology/Business/Test/ThrombocytopeniaProfile/ThrombocytopeniaProfileTest.cs
@@ -10,15 +10,14 @@ namespace YellowstonePathology.Business.Test.ThrombocytopeniaProfile
         public ThrombocytopeniaProfileTest()
         {
             this.m_PanelSetId = 21;
-            this.m_PanelSetName = "Thrombocytopenia Profile";
-            //this.m_TestId = 13;
+            this.m_PanelSetName = "Thrombocytopenia Profile";            
 			this.m_AllowMultiplePerAccession = true;
             this.m_ExpectedDuration = new TimeSpan(24, 0, 0);
             this.m_EpicDistributionIsImplemented = true;
             this.m_WordDocumentClassName = typeof(YellowstonePathology.Business.Test.ThombocytopeniaProfile.ThombocytopeniaProfileWordDocument).AssemblyQualifiedName;
 
             string taskDescription = "Perform thrombocytopenia profile testing.";
-			this.m_TaskCollection.Add(new YellowstonePathology.Business.Task.Model.TaskFedexShipment(YellowstonePathology.Business.Task.Model.TaskAssignment.Flow, taskDescription, new Facility.Model.NeogenomicsIrvine()));
+			this.m_TaskCollection.Add(new YellowstonePathology.Business.Task.Model.Task(YellowstonePathology.Business.Task.Model.TaskAssignment.Flow, taskDescription));
 
             this.m_HasProfessionalComponent = false;
             this.m_ProfessionalComponentFacility = null;
