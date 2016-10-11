6bbb9ea6f5c58aba04b8da623f0e1dce435ee340
SidHarder <softwarenavigator@gmail.com>
2016-10-11 14:39:08 -0600

    zpl histology block

---
+++ YellowstonePathology/Business/Label.Model/HistologyBlockPaperZPLLabel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowstonePathology.Business.Label.Model
+{
+    public class HistologyBlockPaperZPLLabel
+    {
+        public HistologyBlockPaperZPLLabel()
+        {
+
+        }
+
+        public static string GetCommands(YellowstonePathology.Business.BarcodeScanning.HistologyBlock histologyBlock, string patientInitials, string blockId)
+        {
+            StringBuilder result = new StringBuilder();
+            
+            //string line1 = containerId.Substring(0, 14);
+            //string line2 = containerId.Substring(14, 14);
+            //string line3 = containerId.Substring(28);
+
+
+            //result.Append("^FO" + (xOffset + 85) + ",020^AUN,50,50^FD" + "YPI" + "^FS");
+            //result.Append("^FO" + (xOffset + 75) + ",080^BXN,04,200^FD" + containerId + "^FS");
+            //result.Append("^FO" + (xOffset + 30) + ",190^FB190,1,0,C,0^ADN,20^FD" + line1 + "^FS");
+            //result.Append("^FO" + (xOffset + 30) + ",210^FB190,1,0,C,0^ADN,20^FD" + line2 + "^FS");
+            //result.Append("^FO" + (xOffset + 25) + ",240^FB190,1,0,C,0^ARN,18^FD" + line3 + "^FS");
+
+            return result.ToString();
+        }        
+    }
+}
---
+++ YellowstonePathology/Business/PanelSet.Model/ShipMaterialTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YellowstonePathology.Business.PanelSet.Model
+{
+    public class ShipMaterialTest : YellowstonePathology.Business.PanelSet.Model.PanelSet
+	{
+		public ShipMaterialTest()
+		{
+			this.m_PanelSetId = 244;
+			this.m_PanelSetName = "Ship Material";
+            this.m_Abbreviation = "SHPMTRL";			
+			this.m_HasTechnicalComponent = false;			
+            this.m_HasProfessionalComponent = false;
+			this.m_ResultDocumentSource = YellowstonePathology.Business.PanelSet.Model.ResultDocumentSourceEnum.None;
+            this.m_ReportNoLetter = new YellowstonePathology.Business.ReportNoLetterR();
+            this.m_Active = true;
+            this.m_ExpectedDuration = TimeSpan.FromDays(1);
+            this.m_WordDocumentClassName = typeof(YellowstonePathology.Business.Document.ReferenceLabReport).AssemblyQualifiedName;
+
+            this.m_AllowMultiplePerAccession = true;                        
+			
+            string taskDescription = "Ship materials";
+			this.m_TaskCollection.Add(new YellowstonePathology.Business.Task.Model.TaskFedexShipment(YellowstonePathology.Business.Task.Model.TaskAssignment.Histology, taskDescription, new Facility.Model.NullFacility()));            
+		}
+	}
+}
