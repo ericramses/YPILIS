c1dcfa93e7ad658219a5352f861177fdace55ac7
SidHarder <softwarenavigator@gmail.com>
2016-10-13 14:00:58 -0600

    zpl

---
+++ YellowstonePathology/Business/Label.Model/HistologySlidePaperZPLLabel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowstonePathology.Business.Label.Model
+{
+    public class HistologySlidePaperZPLLabel
+    {
+        public HistologySlidePaperZPLLabel()
+        {
+
+        }
+
+        public static string GetCommands(string slideOrderId)
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append("^XA");        
+            
+            //result.Append("^FO" + 30 + ",090^BXN,08,200^FD" + histologyBlock.AliquotOrderId + "^FS");
+            //result.Append("^FO" + 30 + ",040^ATN,40,40^FD" + masterAccessionNo + "^FS");
+            //result.Append("^FO" + 30 + ",220^ARN,25,25^FD" + patientInitials + "^FS");
+            //result.Append("^FO" + 175 + ",220^ARN,25,25^FD" + blockId + "^FS");
+
+            result.Append("^XZ");
+            return result.ToString();
+        }        
+    }
+}
--- YellowstonePathology/UI/AdministrationWorkspace.xaml.cs
+++ YellowstonePathology/UI/AdministrationWorkspace.xaml.cs
@@ -999,9 +999,11 @@ namespace YellowstonePathology.UI
         private void ButtonRunMethod_Click(object sender, RoutedEventArgs e)
         {
             Business.Label.Model.ZPLPrinter printer = new Business.Label.Model.ZPLPrinter("10.1.1.21");
-            YellowstonePathology.Business.BarcodeScanning.BarcodeVersion1 barcode = new Business.BarcodeScanning.BarcodeVersion1("HBLK16-25894.1A");
-            YellowstonePathology.Business.BarcodeScanning.HistologyBlock histologyBlock = Business.BarcodeScanning.HistologyBlock.Parse(barcode);
-            string commands = Business.Label.Model.HistologyBlockPaperZPLLabel.GetCommands(histologyBlock, "MM", "1A", "16-25894");
+            //YellowstonePathology.Business.BarcodeScanning.BarcodeVersion1 barcode = new Business.BarcodeScanning.BarcodeVersion1("HBLK16-25894.1A");
+            //YellowstonePathology.Business.BarcodeScanning.HistologyBlock histologyBlock = Business.BarcodeScanning.HistologyBlock.Parse(barcode);
+            //string commands = Business.Label.Model.HistologyBlockPaperZPLLabel.GetCommands(histologyBlock, "MM", "1A", "16-25894");
+
+            string commands = YellowstonePathology.Business.Label.Model.HistologySlidePaperZPLLabel.GetCommands("16-12345.1A1");            
             printer.Print(commands);            
         }        
 
