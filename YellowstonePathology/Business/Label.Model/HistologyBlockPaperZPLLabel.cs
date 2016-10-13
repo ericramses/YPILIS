3c8a1a57ebe364db48f422733abb5df260a96f5a
Mark Schuetze <markjschuetze@gmail.com>
2016-10-13 14:48:23 -0600

    histologyslide zpl

--- YellowstonePathology/Business/Label.Model/HistologyBlockPaperZPLLabel.cs
+++ YellowstonePathology/Business/Label.Model/HistologyBlockPaperZPLLabel.cs
@@ -17,18 +17,12 @@ namespace YellowstonePathology.Business.Label.Model
         {
             StringBuilder result = new StringBuilder();
 
-            result.Append("^XA");        
-            
+            result.Append("^XA");         
             result.Append("^FO" + 30 + ",090^BXN,08,200^FD" + histologyBlock.AliquotOrderId + "^FS");
             result.Append("^FO" + 30 + ",040^ATN,40,40^FD" + masterAccessionNo + "^FS");
             result.Append("^FO" + 30 + ",220^ARN,25,25^FD" + patientInitials + "^FS");
             result.Append("^FO" + 175 + ",220^ARN,25,25^FD" + blockId + "^FS");
-
-
-
-
             result.Append("^XZ");
-
 
             return result.ToString();
         }        
