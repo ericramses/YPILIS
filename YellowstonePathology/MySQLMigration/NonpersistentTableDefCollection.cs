717e3a35a3c3cc92f9c4501fa85c882f237d183a
WilliamCopland <william.copland@ypii.com>
2016-11-21 19:03:23 -0700

    Whoops forgot to add the files

---
+++ YellowstonePathology/MySQLMigration/NonpersistentTableDefCollection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace YellowstonePathology.MySQLMigration
+{
+    public class NonpersistentTableDefCollection : ObservableCollection<NonpersistentTableDef>
+    {
+        public NonpersistentTableDefCollection()
+        { }
+
+        public static NonpersistentTableDefCollection GetAll()
+        {
+            NonpersistentTableDefCollection result = new NonpersistentTableDefCollection();
+            result.Add(new NonpersistentTableDefPanelSet());
+
+            return result;
+        }
+    }
+}
