f9bbab5884149164a4d3e336267a5e92fbbbcfb6
WilliamCopland <william.copland@ypii.com>
2016-12-21 10:39:00 -0700

    Missed a table

--- YellowstonePathology/MySQLMigration/NonpersistentTableDefCollection.cs
+++ YellowstonePathology/MySQLMigration/NonpersistentTableDefCollection.cs
@@ -23,6 +23,7 @@ namespace YellowstonePathology.MySQLMigration
             result.Add(new NonpersistentTableDefCytologyScreeningImpression());
             result.Add(new NonpersistentTableDefFlowCommentV2());
             result.Add(new NonpersistentTableDefFlowMarkerPanel());
+            result.Add(new NonpersistentTableDefFlowPanel());
             result.Add(new NonpersistentTableDefICD9Code());
             result.Add(new NonpersistentTableDefImmunoComment());
             result.Add(new NonpersistentTableDefMarkers());
