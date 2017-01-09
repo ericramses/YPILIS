30be45429711018c2e44ff26045d15c6b1604218
SidHarder <softwarenavigator@gmail.com>
2017-01-09 13:39:21 -0700

    adding pid sement

---
+++ YellowstonePathology/Business/Patient.Model/Address.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace YellowstonePathology.Business.Patient.Model
+{
+    public class Address : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string m_PAddress1;
+        private string m_PAddress2;
+        private string m_PCity;
+        private string m_PState;
+        private string m_PZipCode;
+
+        public Address()
+        {
+
+        }
+        
+        public string PAddress1
+        {
+            get { return this.m_PAddress1; }
+            set
+            {
+                if (this.m_PAddress1 != value)
+                {
+                    this.m_PAddress1 = value;
+                    this.NotifyPropertyChanged("PAddress1");
+                }
+            }
+        }
+        
+        public string PAddress2
+        {
+            get { return this.m_PAddress2; }
+            set
+            {
+                if (this.m_PAddress2 != value)
+                {
+                    this.m_PAddress2 = value;
+                    this.NotifyPropertyChanged("PAddress2");
+                }
+            }
+        }
+        
+        public string PCity
+        {
+            get { return this.m_PCity; }
+            set
+            {
+                if (this.m_PCity != value)
+                {
+                    this.m_PCity = value;
+                    this.NotifyPropertyChanged("PCity");
+                }
+            }
+        }
+      
+        public string PState
+        {
+            get { return this.m_PState; }
+            set
+            {
+                if (this.m_PState != value)
+                {
+                    this.m_PState = value;
+                    this.NotifyPropertyChanged("PState");
+                }
+            }
+        }
+        
+        public string PZipCode
+        {
+            get { return this.m_PZipCode; }
+            set
+            {
+                if (this.m_PZipCode != value)
+                {
+                    this.m_PZipCode = value;
+                    this.NotifyPropertyChanged("PZipCode");
+                }
+            }
+        }
+
+        public string DisplayString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine("Address: " + this.m_PAddress1);
+            if(string.IsNullOrEmpty(this.m_PAddress2) == false)
+            {
+                result.AppendLine(this.m_PAddress2);
+            }            
+            result.AppendLine(this.m_PCity + " " + this.m_PState + ", " + this.m_PZipCode);
+            return result.ToString();
+        }
+
+        public void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+    }
+}
