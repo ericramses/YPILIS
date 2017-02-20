6f913add55d5ecb5d0a02c31faa7879dd62a9a26
WilliamCopland <william.copland@ypii.com>
2017-02-20 09:45:56 -0700

    Add CXCR4 Mutation Analysis as Arup test

---
+++ YellowstonePathology/Business/PanelSet.Model/CXCR4MutationAnalysisTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowstonePathology.Business.PanelSet.Model
+{
+    public class CXCR4MutationAnalysisTest : YellowstonePathology.Business.PanelSet.Model.PanelSet
+    {
+        public CXCR4MutationAnalysisTest()
+        {
+            this.m_PanelSetId = 253;
+            this.m_PanelSetName = "CXCR4 Mutation Analysis";
+            this.m_CaseType = YellowstonePathology.Business.CaseType.Molecular;
+            this.m_HasTechnicalComponent = true;
+            this.m_HasProfessionalComponent = true;
+            this.m_ResultDocumentSource = ResultDocumentSourceEnum.PublishedDocument;
+            this.m_ReportNoLetter = new YellowstonePathology.Business.ReportNoLetterR();
+            this.m_Active = false;
+            this.m_WordDocumentClassName = typeof(YellowstonePathology.Business.Document.ReferenceLabReport).AssemblyQualifiedName;
+
+
+            this.m_AllowMultiplePerAccession = true;
+
+            string taskDescription = "Gather materials and send to ARUP.";
+            this.m_TaskCollection.Add(new YellowstonePathology.Business.Task.Model.TaskFedexShipment(YellowstonePathology.Business.Task.Model.TaskAssignment.Molecular, taskDescription, new Facility.Model.ARUP()));
+
+            this.m_TechnicalComponentFacility = new YellowstonePathology.Business.Facility.Model.ARUP();
+            this.m_ProfessionalComponentFacility = new YellowstonePathology.Business.Facility.Model.ARUP();
+
+            this.m_TechnicalComponentBillingFacility = new YellowstonePathology.Business.Facility.Model.ARUP();
+            this.m_ProfessionalComponentBillingFacility = new YellowstonePathology.Business.Facility.Model.ARUP();
+
+            this.m_UniversalServiceIdCollection.Add(new YellowstonePathology.Business.ClientOrder.Model.UniversalServiceDefinitions.UniversalServiceMOLEGEN());
+        }
+    }
+}
