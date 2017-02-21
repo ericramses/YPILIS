04e9413ff33e40c16eebd1f6f445873a15389cd1
SidHarder <softwarenavigator@gmail.com>
2017-02-21 09:58:20 -0700

    adding assignment scan

---
+++ YellowstonePathology/UI/AssignmentScanCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace YellowstonePathology.UI
+{
+    public class AssignmentScanCollection : ObservableCollection<AssignmentScan>
+    {
+        public AssignmentScanCollection()
+        {
+
+        }
+
+        public bool Exists(string scanId)
+        {
+            bool result = false;
+            foreach(AssignmentScan item in this)
+            {
+                if(item.ScanId == scanId)
+                {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
