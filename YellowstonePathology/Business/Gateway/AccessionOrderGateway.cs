0fc4913b38e374e2b75b30bb1af311249bd7831c
WilliamCopland <william.copland@ypii.com>
2017-03-28 12:03:47 -0600

    Fix Surgical Master Log fill

--- YellowstonePathology/Business/Gateway/AccessionOrderGateway.cs
+++ YellowstonePathology/Business/Gateway/AccessionOrderGateway.cs
@@ -1061,33 +1061,35 @@ namespace YellowstonePathology.Business.Gateway
 		{
 			Surgical.SurgicalMasterLogList result = new Surgical.SurgicalMasterLogList();
             MySqlCommand cmd = new MySqlCommand();
-            cmd.CommandText = "declare rpts table " +
-                "( " +
-                "AccessionTime datetime, " +
-                "ReportNo varchar(20), " +
-                "AccessioningFacilityId varchar(100), " +
-                "PFirstName varchar(100), " +
-                "PLastName varchar(100), " +
-                "PBirthdate datetime, " +
-                "PhysicianName varchar(100), " +
-                "ClientName varchar(100), " +
-                "AliquotCount int " +
-                ") " +
-                "insert rpts " +
-                "SELECT Distinct a.AccessionTime, pso.ReportNo, a.AccessioningFacilityId, a.PFirstName, a.PLastName, " +
-                "a.PBirthdate, a.PhysicianName, a.ClientName, Count(*) AliquotCount " +
-                "FROM tblAccessionOrder a JOIN tblPanelSetOrder pso ON a.MasterAccessionNo = pso.MasterAccessionNo " +
-                "JOIN tblSpecimenORder so on a.MasterAccessionNo = so.MasterAccessionNo " +
-                "LEFT OUTER JOIN tblAliquotOrder ao on so.SpecimenOrderId = ao.SpecimenOrderId " +
-                "WHERE AccessionDate = @ReportDate and pso.PanelSetId in (13, 50)  " +
-                " group by a.AccessionTime, pso.ReportNo, a.AccessioningFacilityId, a.PFirstName, a.PLastName, " +
-                "a.PBirthdate, a.PhysicianName, a.ClientName " +
-                "Order By AccessionTime " +
-                "SELECT rpts.* From rpts rpts Order By AccessionTime " +
-                "Select ssr.DiagnosisId, so.Description, ssr.ReportNo " +
-                "FROM tblSurgicalSpecimen ssr " +
-                "JOIN tblSpecimenOrder so ON ssr.SpecimenOrderId = so.SpecimenOrderId " +
-                "join rpts rpts on ssr.ReportNo = rpts.ReportNo order by 1;";
+            cmd.CommandText = "create temporary table if not exists rpts " +
+            "( " +
+            "AccessionTime datetime, " +
+            "ReportNo varchar(20), " +
+            "AccessioningFacilityId varchar(100), " +
+            "PFirstName varchar(100), " +
+            "PLastName varchar(100), " +
+            "PBirthdate datetime, " +
+            "PhysicianName varchar(100), " +
+            "ClientName varchar(100), " +
+            "AliquotCount int " +
+            ") " +
+            "as " +
+            "SELECT Distinct a.AccessionTime, pso.ReportNo, a.AccessioningFacilityId, a.PFirstName, a.PLastName,  " +
+            "a.PBirthdate, a.PhysicianName, a.ClientName, Count(*) AliquotCount " +
+            "FROM tblAccessionOrder a JOIN tblPanelSetOrder pso ON a.MasterAccessionNo = pso.MasterAccessionNo " +
+            "JOIN tblSpecimenOrder so on a.MasterAccessionNo = so.MasterAccessionNo " +
+            "LEFT OUTER JOIN tblAliquotOrder ao on so.SpecimenOrderId = ao.SpecimenOrderId " +
+            "WHERE AccessionDate = '2017-3-24' and pso.PanelSetId in (13, 50) " +
+            "group by a.AccessionTime, pso.ReportNo, a.AccessioningFacilityId, a.PFirstName, a.PLastName, " +
+            "a.PBirthdate, a.PhysicianName, a.ClientName " +
+            "Order By AccessionTime; " +
+            "SELECT rpts.*From rpts rpts Order By AccessionTime; " +
+            "Select ssr.DiagnosisId, so.Description, ssr.ReportNo " +
+            "FROM tblSurgicalSpecimen ssr " +
+            "JOIN tblSpecimenOrder so ON ssr.SpecimenOrderId = so.SpecimenOrderId " +
+            "join rpts rpts on ssr.ReportNo = rpts.ReportNo order by 1; " +
+            "drop temporary table rpts; ";
+
             cmd.CommandType = CommandType.Text;
             cmd.Parameters.AddWithValue("@ReportDate", reportDate.ToShortDateString());
             using (MySqlConnection cn = new MySqlConnection(YellowstonePathology.Properties.Settings.Default.CurrentConnectionString))
