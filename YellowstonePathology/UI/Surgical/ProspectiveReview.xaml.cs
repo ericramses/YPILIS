8608a700374b25fe538b7fa07647981f27992718
SidHarder <softwarenavigator@gmail.com>
2017-04-05 09:47:30 -0600

    review

---
+++ YellowstonePathology/UI/Surgical/ProspectiveReview.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace YellowstonePathology.UI.Surgical
+{
+    /// <summary>
+    /// Interaction logic for ProspectiveReview.xaml
+    /// </summary>
+    public partial class ProspectiveReview : Window
+    {
+        YellowstonePathology.Business.Test.AccessionOrder m_AccessionOrder;
+        List<YellowstonePathology.Business.Test.PanelOrder> m_PanelOrderItems;
+
+        public ProspectiveReview(YellowstonePathology.Business.Test.AccessionOrder accessonOrderItem)
+        {
+            this.m_PanelOrderItems = new List<YellowstonePathology.Business.Test.PanelOrder>();
+            this.m_AccessionOrder = accessonOrderItem;
+            InitializeComponent();
+            this.DataContext = this;            
+        }
+
+        public YellowstonePathology.Business.Test.AccessionOrder AccessionOrder
+        {
+            get { return this.m_AccessionOrder; }
+            set { this.m_AccessionOrder = value; }
+        }
+
+        //public List<YellowstonePathology.Business.Test.PanelOrder> PanelOrderItems
+		public List<YellowstonePathology.Business.Interface.IPanelOrder> PanelOrderItems
+        {
+            get 
+            {
+				//List<YellowstonePathology.Business.Test.PanelOrder> panelOrderItems = null;
+				List<YellowstonePathology.Business.Interface.IPanelOrder> panelOrderItems = null;
+                var query = from pso in this.m_AccessionOrder.PanelSetOrderCollection
+                            from po in pso.PanelOrderCollection
+                            where po.PanelId == 33
+                            select po;
+                //if (query.Count<YellowstonePathology.Business.Test.PanelOrder>() != 0)
+				if (query.Count<YellowstonePathology.Business.Interface.IPanelOrder>() != 0)
+				{
+					panelOrderItems = query.ToList<YellowstonePathology.Business.Interface.IPanelOrder>();
+					//panelOrderItems = query.ToList<YellowstonePathology.Business.Test.PanelOrder>();
+                }
+                return panelOrderItems;
+            }            
+        }
+    }
+}
