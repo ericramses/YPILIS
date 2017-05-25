c5c6a4ce4ee027bc78dddd591a892716cead6744
William Copland <William.Copland@YPIILab.YPII.com>
2017-05-25 09:47:43 -0600

    And now for the file

---
+++ YellowstonePathology/Business/Test/Miprofile/MiprofileTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace YellowstonePathology.Business.Test.Miprofile
+{
+    public class MiprofileTest : YellowstonePathology.Business.PanelSet.Model.PanelSet
+    {
+        public MiprofileTest()
+        {
+            this.m_PanelSetId = 265;
+            this.m_PanelSetName = "Miprofile";
+            this.m_Abbreviation = "Miprofile";
+            this.m_CaseType = YellowstonePathology.Business.CaseType.Molecular;
+            this.m_HasTechnicalComponent = true;
+            this.m_HasProfessionalComponent = true;
+            this.m_ResultDocumentSource = YellowstonePathology.Business.PanelSet.Model.ResultDocumentSourceEnum.PublishedDocument;
+            this.m_ReportNoLetter = new YellowstonePathology.Business.ReportNoLetterR();
+            this.m_Active = true;
+
+            this.m_PanelSetOrderClassName = typeof(YellowstonePathology.Business.Test.PanelSetOrder).AssemblyQualifiedName;
+            this.m_WordDocumentClassName = typeof(YellowstonePathology.Business.Document.ReferenceLabReport).AssemblyQualifiedName;
+            this.m_AllowMultiplePerAccession = true;
+
+            string taskDescription = "Collect paraffin block from Histology and send to Caris Life Sciences.";
+            this.m_TaskCollection.Add(new YellowstonePathology.Business.Task.Model.TaskFedexShipment(YellowstonePathology.Business.Task.Model.TaskAssignment.Molecular, taskDescription, new Facility.Model.NeogenomicsIrvine()));
+
+            this.m_TechnicalComponentFacility = new YellowstonePathology.Business.Facility.Model.CarisLifeSciences();
+            this.m_TechnicalComponentBillingFacility = new YellowstonePathology.Business.Facility.Model.YellowstonePathologyInstituteBillings();
+
+            this.m_ProfessionalComponentFacility = new YellowstonePathology.Business.Facility.Model.CarisLifeSciences();
+            this.m_ProfessionalComponentBillingFacility = new YellowstonePathology.Business.Facility.Model.YellowstonePathologyInstituteBillings();
+
+            this.m_UniversalServiceIdCollection.Add(new YellowstonePathology.Business.ClientOrder.Model.UniversalServiceDefinitions.UniversalServiceMOLEGEN());
+        }
+    }
+}
