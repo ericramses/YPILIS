0bb05608aeb08f91d078ec525cf71b7bdb5b10e1
William Copland <William.Copland@YPIILab.YPII.com>
2017-06-26 10:21:28 -0600

    Add tests - ChromosomeAnalysusBreakage and Her2BreastEquivocalFishPanel

---
+++ YellowstonePathology/Business/Test/ChromosomeAnalysisBreakage/ChromosomeAnalysisBreakageTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowstonePathology.Business.Test.ChromosomeAnalysisBreakage
+{
+    public class ChromosomeAnalysisBreakageTest : YellowstonePathology.Business.PanelSet.Model.PanelSet
+    {
+        public ChromosomeAnalysisBreakageTest()
+        {
+            this.m_PanelSetId = 270;
+            this.m_PanelSetName = "Chromosome Analysis Breakage";
+            this.m_Abbreviation = "Chromosome Analysis Breakage";
+            this.m_CaseType = YellowstonePathology.Business.CaseType.Molecular;
+            this.m_HasTechnicalComponent = true;
+            this.m_HasProfessionalComponent = true;
+            this.m_ResultDocumentSource = YellowstonePathology.Business.PanelSet.Model.ResultDocumentSourceEnum.PublishedDocument;
+            this.m_ReportNoLetter = new YellowstonePathology.Business.ReportNoLetterR();
+            this.m_Active = true;
+
+            this.m_PanelSetOrderClassName = typeof(YellowstonePathology.Business.Test.PanelSetOrder).AssemblyQualifiedName;
+            this.m_WordDocumentClassName = typeof(YellowstonePathology.Business.Document.ReferenceLabReport).AssemblyQualifiedName;
+            this.m_AllowMultiplePerAccession = true;
+
+            string taskDescription = "Collect material and send to ARUP.";
+            this.m_TaskCollection.Add(new YellowstonePathology.Business.Task.Model.TaskFedexShipment(YellowstonePathology.Business.Task.Model.TaskAssignment.Molecular, taskDescription, new Facility.Model.ARUP()));
+
+            this.m_TechnicalComponentFacility = new YellowstonePathology.Business.Facility.Model.ARUP();
+            this.m_TechnicalComponentBillingFacility = new YellowstonePathology.Business.Facility.Model.YellowstonePathologyInstituteBillings();
+
+            this.m_ProfessionalComponentFacility = new YellowstonePathology.Business.Facility.Model.YellowstonePathologistBillings();
+            this.m_ProfessionalComponentBillingFacility = new YellowstonePathology.Business.Facility.Model.YellowstonePathologyInstituteBillings();
+
+            this.m_UniversalServiceIdCollection.Add(new YellowstonePathology.Business.ClientOrder.Model.UniversalServiceDefinitions.UniversalServiceMOLEGEN());
+        }
+    }
+}
