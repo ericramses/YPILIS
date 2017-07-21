a4011ba26695016b2b6616e0e80580b327e39c87
William Copland <William.Copland@YPIILab.YPII.com>
2017-07-21 09:38:57 -0600

    add new test and test order

---
+++ YellowstonePathology/Business/Test/BRAFMutationAnalysis/BRAFMutationAnalysisTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowstonePathology.Business.Test.BRAFMutationAnalysis
+{
+    public class BRAFMutationAnalysisTest : YellowstonePathology.Business.PanelSet.Model.PanelSet
+    {
+    }
+}
