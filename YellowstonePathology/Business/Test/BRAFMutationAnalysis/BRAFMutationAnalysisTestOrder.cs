2a4b023903522f9a72e268abe3f6542e9de95752
William Copland <William.Copland@YPIILab.YPII.com>
2017-07-24 13:03:30 -0600

    Add Braf Mutation analyais result page and path

--- YellowstonePathology/Business/Test/BRAFMutationAnalysis/BRAFMutationAnalysisTestOrder.cs
+++ YellowstonePathology/Business/Test/BRAFMutationAnalysis/BRAFMutationAnalysisTestOrder.cs
@@ -3,10 +3,174 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YellowstonePathology.Business.Persistence;
 
 namespace YellowstonePathology.Business.Test.BRAFMutationAnalysis
 {
+    [PersistentClass("tblBRAFMutationAnalysisTestOrder", "tblPanelSetOrder", "YPIDATA")]
     public class BRAFMutationAnalysisTestOrder : YellowstonePathology.Business.Test.PanelSetOrder
     {
+        private string m_Result;
+        private string m_Interpretation;
+        private string m_Comment;
+        private string m_Indication;
+        private string m_IndicationComment;
+        private string m_TumorNucleiPercentage;
+        private string m_Method;
+        private string m_ReportDisclaimer;
+
+        public BRAFMutationAnalysisTestOrder()
+        {
+
+        }
+
+        public BRAFMutationAnalysisTestOrder(string masterAccessionNo, string reportNo, string objectId,
+            YellowstonePathology.Business.PanelSet.Model.PanelSet panelSet,
+            YellowstonePathology.Business.Interface.IOrderTarget orderTarget,
+            bool distribute)
+			: base(masterAccessionNo, reportNo, objectId, panelSet, orderTarget, distribute)
+		{
+            this.m_ReportDisclaimer = "";
+        }
+
+        [PersistentProperty()]
+        [PersistentDataColumnProperty(true, "50", "null", "varchar")]
+        public string Result
+        {
+            get { return this.m_Result; }
+            set
+            {
+                if (this.m_Result != value)
+                {
+                    this.m_Result = value;
+                    this.NotifyPropertyChanged("Result");
+                }
+            }
+        }
+
+        [PersistentProperty()]
+        [PersistentDataColumnProperty(true, "5000", "null", "varchar")]
+        public string Interpretation
+        {
+            get { return this.m_Interpretation; }
+            set
+            {
+                if (this.m_Interpretation != value)
+                {
+                    this.m_Interpretation = value;
+                    this.NotifyPropertyChanged("Interpretation");
+                }
+            }
+        }
+
+        [PersistentProperty()]
+        [PersistentDataColumnProperty(true, "500", "null", "varchar")]
+        public string Comment
+        {
+            get { return this.m_Comment; }
+            set
+            {
+                if (this.m_Comment != value)
+                {
+                    this.m_Comment = value;
+                    this.NotifyPropertyChanged("Comment");
+                }
+            }
+        }
+
+        [PersistentProperty()]
+        [PersistentDataColumnProperty(true, "50", "null", "varchar")]
+        public string Indication
+        {
+            get { return this.m_Indication; }
+            set
+            {
+                if (this.m_Indication != value)
+                {
+                    this.m_Indication = value;
+                    this.NotifyPropertyChanged("Indication");
+                }
+            }
+        }
+
+        [PersistentProperty()]
+        [PersistentDataColumnProperty(true, "500", "null", "varchar")]
+        public string IndicationComment
+        {
+            get { return this.m_IndicationComment; }
+            set
+            {
+                if (this.m_IndicationComment != value)
+                {
+                    this.m_IndicationComment = value;
+                    this.NotifyPropertyChanged("IndicationComment");
+                }
+            }
+        }
+
+        [PersistentProperty()]
+        [PersistentDataColumnProperty(true, "50", "null", "varchar")]
+        public string TumorNucleiPercentage
+        {
+            get { return this.m_TumorNucleiPercentage; }
+            set
+            {
+                if (this.m_TumorNucleiPercentage != value)
+                {
+                    this.m_TumorNucleiPercentage = value;
+                    this.NotifyPropertyChanged("TumorNucleiPercentage");
+                }
+            }
+        }
+
+        [PersistentProperty()]
+        [PersistentDataColumnProperty(true, "5000", "null", "varchar")]
+        public string Method
+        {
+            get { return this.m_Method; }
+            set
+            {
+                if (this.m_Method != value)
+                {
+                    this.m_Method = value;
+                    this.NotifyPropertyChanged("Method");
+                }
+            }
+        }
+
+        [PersistentProperty()]
+        [PersistentDataColumnProperty(true, "1000", "null", "varchar")]
+        public string ReportDisclaimer
+        {
+            get { return this.m_ReportDisclaimer; }
+            set
+            {
+                if (this.m_ReportDisclaimer != value)
+                {
+                    this.m_ReportDisclaimer = value;
+                    this.NotifyPropertyChanged("ReportDisclaimer");
+                }
+            }
+        }
+
+        public override string ToResultString(YellowstonePathology.Business.Test.AccessionOrder accessionOrder)
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append("Result: ");
+            result.AppendLine(this.m_Result);
+            result.AppendLine();
+            result.Append("Result Comment: ");
+            result.AppendLine(this.m_Comment);
+            result.AppendLine();
+            result.Append("Interpretation: ");
+            result.AppendLine(this.m_Interpretation);
+            result.AppendLine();
+            result.Append("Indication: ");
+            result.AppendLine(this.m_Indication);
+            result.AppendLine();
+            result.Append("Tumor Nuclei Percent: ");
+            result.AppendLine(this.m_TumorNucleiPercentage);
+            return result.ToString();
+        }
     }
 }
