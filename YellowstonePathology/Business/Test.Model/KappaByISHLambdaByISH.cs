a14ec9a84b3f4340add270c6dc05d20d7f2b3f54
William Copland <William.Copland@YPIILab.YPII.com>
2017-09-06 12:34:44 -0600

    Add new test Kappa by ish - lambda by ish, remove panelset created in error

---
+++ YellowstonePathology/Business/Test.Model/KappaByISHLambdaByISH.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowstonePathology.Business.Test.Model
+{
+    public class KappaByISHLambdaByISH : ImmunoHistochemistryTest
+    {
+        public KappaByISHLambdaByISH()
+        {
+            this.m_TestId = 360;
+            this.m_TestName = "Kappa by ISH/Lambda by ISH";
+            this.m_TestAbbreviation = "Kappa/LambdaByISH";
+            this.m_Active = true;
+            this.m_NeedsAcknowledgement = true;
+        }
+    }
+}
