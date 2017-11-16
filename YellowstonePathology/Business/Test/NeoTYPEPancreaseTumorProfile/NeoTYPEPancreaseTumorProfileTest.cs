b86f870299daf94cea984422f07cef1e4b469830
William Copland <william.copland@ypii.com>
2017-11-16 12:16:16 -0700

    Add NeoTYPEPancreaseTumorProfile test and to PanelSetCollection

---
+++ YellowstonePathology/Business/Test/NeoTYPEPancreaseTumorProfile/NeoTYPEPancreaseTumorProfileTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowstonePathology.Business.Test.NeoTYPEPancreaseTumorProfile
+{
+    public class NeoTYPEPancreaseTumorProfileTest : YellowstonePathology.Business.PanelSet.Model.PanelSet
+    {
+        public NeoTYPEPancreaseTumorProfileTest()
+        {
+            this.m_PanelSetId = 288;
+            this.m_PanelSetName = "Neo TYPE Pancrease Tumor Profile";
+            this.m_Abbreviation = "Neo TYPE Pancrease Tumor Profile";
+            this.m_CaseType = YellowstonePathology.Business.CaseType.Molecular;
+            this.m_HasTechnicalComponent = true;
+            this.m_HasProfessionalComponent = true;
+            this.m_ResultDocumentSource = YellowstonePathology.Business.PanelSet.Model.ResultDocumentSourceEnum.PublishedDocument;
+            this.m_ReportNoLetter = new YellowstonePathology.Business.ReportNoLetterR();
+            this.m_Active = true;
+            this.m_AttemptOrderTargetLookup = true;
+            this.m_PanelSetOrderClassName = typeof(YellowstonePathology.Business.Test.PanelSetOrder).AssemblyQualifiedName;
+            this.m_WordDocumentClassName = typeof(YellowstonePathology.Business.Document.ReferenceLabReport).AssemblyQualifiedName;
+            this.m_AllowMultiplePerAccession = true;
+            this.m_ExpectedDuration = new TimeSpan(5, 0, 0, 0);
+
+            string taskDescription = "Gather materials and send out to Neo.";
+            this.m_TaskCollection.Add(new YellowstonePathology.Business.Task.Model.TaskFedexShipment(YellowstonePathology.Business.Task.Model.TaskAssignment.Flow, taskDescription, new Facility.Model.NeogenomicsIrvine()));
+
+            this.m_TechnicalComponentFacility = new YellowstonePathology.Business.Facility.Model.NeogenomicsIrvine();
+            this.m_TechnicalComponentBillingFacility = new YellowstonePathology.Business.Facility.Model.YellowstonePathologyInstituteBillings();
+
+            this.m_ProfessionalComponentFacility = new YellowstonePathology.Business.Facility.Model.NeogenomicsIrvine();
+            this.m_ProfessionalComponentBillingFacility = new YellowstonePathology.Business.Facility.Model.NeogenomicsIrvine();
+
+            this.m_UniversalServiceIdCollection.Add(new YellowstonePathology.Business.ClientOrder.Model.UniversalServiceDefinitions.UniversalServiceMiscellaneous());
+        }
+    }
+}
