98d1eb4e5a2166b2751150af6b6f64f2f49c34c8
SidHarder <softwarenavigator@gmail.com>
2017-12-14 07:59:00 -0700

    setting up redis store

---
+++ YellowstonePathology/Store/AppDataStore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowstonePathology.Store
+{
+    public class AppDataStore
+    {        
+        //private static string MODE = "DEV";
+        private static string MODE = "PROD";     
+
+        public AppDataStore()
+        {
+            if(MODE == "PROD")
+            {
+               
+            }
+            else
+            {
+
+            }
+        }        
+    }
+}
---
+++ YellowstonePathology/Store/RedisStoreProd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace YellowstonePathology.Store
+{
+    public class RedisStoreProd : RedisStore
+    {               
+        public RedisStoreProd()
+        {
+            this.m_Databases.Add(new RedisDB(AppDBNameEnum.CPTCode, 1, RedisServerKub.Instance));
+            this.m_Databases.Add(new RedisDB(AppDBNameEnum.ICDCode, 2, RedisServerKub.Instance));
+            this.m_Databases.Add(new RedisDB(AppDBNameEnum.Stain, 3, RedisServerKub.Instance));
+            this.m_Databases.Add(new RedisDB(AppDBNameEnum.PQRS, 4, RedisServerKub.Instance));
+            this.m_Databases.Add(new RedisDB(AppDBNameEnum.Lock, 5, RedisServerKub.Instance));
+            this.m_Databases.Add(new RedisDB(AppDBNameEnum.VantageSlideScan, 5, RedisServerKub.Instance));
+        }
+    }
+}
