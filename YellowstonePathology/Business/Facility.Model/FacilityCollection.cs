eda665de86df215d17bda06e637daee31f81caf9
SidHarder <softwarenavigator@gmail.com>
2017-12-21 09:05:35 -0700

    adding new facility

--- YellowstonePathology/Business/Facility.Model/FacilityCollection.cs
+++ YellowstonePathology/Business/Facility.Model/FacilityCollection.cs
@@ -129,6 +129,7 @@ namespace YellowstonePathology.Business.Facility.Model
             result.Add(new VirginiaMason());
             result.Add(new UCHLoneTreeBreastCenter());
             result.Add(new Cellnetix());
+            result.Add(new UniversityOfUtahHealthCare());
             return Sort(result);
         }
 
