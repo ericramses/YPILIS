3d99de5c2961f2df29457c7dde095119bca9b864
SidHarder <softwarenavigator@gmail.com>
2018-01-25 09:59:57 -0700

    tcell codes

--- YellowstonePathology/Business/Test/TCellSubsetAnalysis/TCellSubsetAnalysisTest.cs
+++ YellowstonePathology/Business/Test/TCellSubsetAnalysis/TCellSubsetAnalysisTest.cs
@@ -34,10 +34,9 @@ namespace YellowstonePathology.Business.Test.TCellSubsetAnalysis
             
             YellowstonePathology.Business.Billing.Model.PanelSetCptCode cpt86359 = new YellowstonePathology.Business.Billing.Model.PanelSetCptCode(Store.AppDataStore.Instance.CPTCodeCollection.GetClone("86359", null), 1);
             YellowstonePathology.Business.Billing.Model.PanelSetCptCode cpt86360 = new YellowstonePathology.Business.Billing.Model.PanelSetCptCode(Store.AppDataStore.Instance.CPTCodeCollection.GetClone("86360", null), 1);
-            YellowstonePathology.Business.Billing.Model.PanelSetCptCode cpt88184 = new YellowstonePathology.Business.Billing.Model.PanelSetCptCode(Store.AppDataStore.Instance.CPTCodeCollection.GetClone("88184", null), 1);
+            
             this.m_PanelSetCptCodeCollection.Add(cpt86359);
-            this.m_PanelSetCptCodeCollection.Add(cpt86360);
-            this.m_PanelSetCptCodeCollection.Add(cpt88184);
+            this.m_PanelSetCptCodeCollection.Add(cpt86360);            
 
             string taskDescription = "Gather materials and perform test.";
 			this.m_TaskCollection.Add(new YellowstonePathology.Business.Task.Model.Task(YellowstonePathology.Business.Task.Model.TaskAssignment.Flow, taskDescription));
