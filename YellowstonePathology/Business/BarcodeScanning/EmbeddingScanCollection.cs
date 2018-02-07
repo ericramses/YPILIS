773c8c30b027fde35224af92c7129fb69f60d8ed
William Copland <william.copland@ypii.com>
2018-02-07 10:13:33 -0700

    Update scan if aliquot order id scanned on same day, insert otherwise

--- YellowstonePathology/Business/BarcodeScanning/EmbeddingScanCollection.cs
+++ YellowstonePathology/Business/BarcodeScanning/EmbeddingScanCollection.cs
@@ -29,15 +29,26 @@ namespace YellowstonePathology.Business.BarcodeScanning
         public EmbeddingScan HandleScan(string aliquotOrderId, DateTime processorStartTime, TimeSpan processorFixationDuration)
         {            
             EmbeddingScan scan = new EmbeddingScan(aliquotOrderId, processorStartTime, processorFixationDuration);
+            EmbeddingScan existingScan = null;
             foreach (EmbeddingScan item in this)
             {
-                if (item.AliquotOrderId == aliquotOrderId)
+                if (item.AliquotOrderId == aliquotOrderId && item.DateScanned.ToShortDateString() == scan.DateScanned.ToShortDateString())
                 {
+                    existingScan = item;
                     this.Remove(item);
                     break;
                 }
             }
-            YellowstonePathology.Business.Gateway.AccessionOrderGateway.SetEmbeddingScan(scan, DateTime.Today);
+
+            if (existingScan == null)
+            {
+                YellowstonePathology.Business.Gateway.AccessionOrderGateway.SetEmbeddingScan(scan, DateTime.Today);
+            }
+            else
+            {
+                scan.UpdateFromExistingScan(existingScan);
+                YellowstonePathology.Business.Gateway.AccessionOrderGateway.UpdateEmbeddingScan(scan, DateTime.Today);
+            }
             this.InsertItem(0, scan);
 
             return scan;
