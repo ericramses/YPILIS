7f48c0a7b852d813d517d42eb8ea9bc672d261b1
William Copland <william.copland@ypii.com>
2018-02-12 12:18:21 -0700

    set external order ids on existing Accession - add the new files A?

---
+++ YellowstonePathology/Business/ClientOrder.Model/ExternalOrderIds.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace YellowstonePathology.Business.ClientOrder.Model
+{
+    public class ExternalOrderIds
+    {
+        private string m_ExternalOrderId;
+        private int m_PanelSetId;
+
+        public ExternalOrderIds() { }
+
+        public ExternalOrderIds(ClientOrder clientOrder)
+        {
+            switch (clientOrder.OrderType)
+            {
+                case "Routine Surgical Pathology":
+                    this.m_PanelSetId = 13;
+                    break;
+                case "CFYPI":
+                    this.m_PanelSetId = 20;
+                    break;
+                case "THINPREP":
+                    this.m_PanelSetId = 15;
+                    break;
+                default:
+                    this.m_PanelSetId = 13;
+                    break;
+            }
+
+            this.m_ExternalOrderId = clientOrder.ExternalOrderId;
+        }
+
+        public string ExternalOrderId
+        {
+            get { return this.m_ExternalOrderId; }
+            set { this.m_ExternalOrderId = value; }
+        }
+
+        public int PanelSetId
+        {
+            get { return this.m_PanelSetId; }
+            set { this.m_PanelSetId = value; }
+        }
+    }
+}
---
+++ YellowstonePathology/Business/ClientOrder.Model/ExternalOrderIdsCollection.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace YellowstonePathology.Business.ClientOrder.Model
+{
+    public class ExternalOrderIdsCollection : ObservableCollection<ExternalOrderIds>
+    {
+        public ExternalOrderIdsCollection() { }
+
+        public ExternalOrderIdsCollection(ClientOrderCollection clientOrders)
+        {
+            foreach (YellowstonePathology.Business.ClientOrder.Model.ClientOrder clientOrder in clientOrders)
+            {
+                if (string.IsNullOrEmpty(clientOrder.ExternalOrderId) == false)
+                {
+                    ExternalOrderIds accessionOrderIds = new Model.ExternalOrderIds(clientOrder);
+                    this.Add(accessionOrderIds);
+                }
+            }
+        }
+
+        public static ExternalOrderIdsCollection FromJSONstring(string jString)
+        {
+            ExternalOrderIdsCollection result = new Model.ExternalOrderIdsCollection();
+            if (string.IsNullOrEmpty(jString) == false)
+            {
+                if (jString[0] == '[')
+                {
+                    result = JsonConvert.DeserializeObject<ExternalOrderIdsCollection>(jString, new JsonSerializerSettings
+                    {
+                        TypeNameHandling = TypeNameHandling.All,
+                        ObjectCreationHandling = ObjectCreationHandling.Replace
+                    });
+                }
+            }
+
+            return result;
+        }
+
+        public string ToJSONString()
+        {
+            string result = JsonConvert.SerializeObject(this);
+            return result;
+        }
+
+
+        public bool Exists(int panelSetId)
+        {
+            bool result = false;
+            foreach (ExternalOrderIds accessionOrderIds in this)
+            {
+                if (accessionOrderIds.PanelSetId == panelSetId)
+                {
+                    result = true;
+                    break;
+                }
+            }
+
+            return result;
+        }
+        public string GetExternalOrderId(int panelSetId)
+        {
+            string result = null;
+            foreach (ExternalOrderIds accessionOrderIds in this)
+            {
+                if (accessionOrderIds.PanelSetId == panelSetId)
+                {
+                    result = accessionOrderIds.ExternalOrderId;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
