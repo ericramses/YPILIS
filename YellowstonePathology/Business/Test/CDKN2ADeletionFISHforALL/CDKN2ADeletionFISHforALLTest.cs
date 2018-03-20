8507753846ab82a4132773e9fd07e30393d4c668
WilliamCopland <william.copland@ypii.com>
2018-03-20 13:03:18 -0600

    Add new Test CDKN2ADeletionFISHforALL

---
+++ YellowstonePathology/Business/Test/CDKN2ADeletionFISHforALL/CDKN2ADeletionFISHforALLTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowstonePathology.Business.Test.CDKN2ADeletionFISHforALL
+{
+    public class CDKN2ADeletionFISHforALLTest : YellowstonePathology.Business.PanelSet.Model.PanelSet
+    {
+        public CDKN2ADeletionFISHforALLTest()
+        {
+            this.m_PanelSetId = 294;
+            this.m_PanelSetName = "CDKN2A (p16) Deletion FISH for ALL";
+            this.m_Abbreviation = "CDKN2A (p16) Deletion FISH for ALL";
+            this.m_CaseType = YellowstonePathology.Business.CaseType.FISH;
+            this.m_HasTechnicalComponent = true;
+            this.m_HasProfessionalComponent = true;
+            this.m_ResultDocumentSource = YellowstonePathology.Business.PanelSet.Model.ResultDocumentSourceEnum.PublishedDocument;
+            this.m_ReportNoLetter = new YellowstonePathology.Business.ReportNoLetterR();
+            this.m_Active = true;
+
+            this.m_PanelSetOrderClassName = typeof(YellowstonePathology.Business.Test.PanelSetOrder).AssemblyQualifiedName;
+            this.m_WordDocumentClassName = typeof(YellowstonePathology.Business.Document.ReferenceLabReport).AssemblyQualifiedName;
+            this.m_AllowMultiplePerAccession = true;
+
+            string taskDescription = "Collect paraffin block from Histology and send to Neo.";
+            this.m_TaskCollection.Add(new YellowstonePathology.Business.Task.Model.TaskFedexShipment(YellowstonePathology.Business.Task.Model.TaskAssignment.Molecular, taskDescription, new Facility.Model.NeogenomicsIrvine()));
+
+            this.m_TechnicalComponentFacility = new YellowstonePathology.Business.Facility.Model.NeogenomicsIrvine();
+            this.m_TechnicalComponentBillingFacility = new YellowstonePathology.Business.Facility.Model.YellowstonePathologyInstituteBillings();
+
+            this.m_ProfessionalComponentFacility = new YellowstonePathology.Business.Facility.Model.YellowstonePathologistBillings();
+            this.m_ProfessionalComponentBillingFacility = new YellowstonePathology.Business.Facility.Model.YellowstonePathologyInstituteBillings();
+
+            this.m_UniversalServiceIdCollection.Add(new YellowstonePathology.Business.ClientOrder.Model.UniversalServiceDefinitions.UniversalServiceMOLEGEN());
+        }
+    }
+}
