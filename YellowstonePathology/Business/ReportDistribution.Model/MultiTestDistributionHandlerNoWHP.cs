998396ff0eb8b3bcb3022177876f07f6eb4c77f8
sidharder <sideharder@gmail.com>
2018-04-11 15:30:18 -0600

    new file

---
+++ YellowstonePathology/Business/ReportDistribution.Model/MultiTestDistributionHandlerNoWHP.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowstonePathology.Business.ReportDistribution.Model
+{
+    public class MultiTestDistributionHandlerNoWHP : MultiTestDistributionHandlerWHP
+    {
+        public MultiTestDistributionHandlerNoWHP(YellowstonePathology.Business.Test.AccessionOrder accessionOrder) 
+            : base(accessionOrder)
+        {
+            this.m_DistributePap = true;
+            this.m_DistributeHPV = true;
+            this.m_DistributeHPV1618 = true;
+            this.m_DistributeNGCT = true;
+            this.m_DistributeTrich = true;
+            this.m_DistributeWHP = false;
+
+            this.m_HoldPap = false;
+            this.m_HoldHPV = false;
+            this.m_HoldHPV1618 = false;
+            this.m_HoldNGCT = false;
+            this.m_HoldTrich = false;
+            this.m_HoldWHP = false;
+        }
+    }
+}
