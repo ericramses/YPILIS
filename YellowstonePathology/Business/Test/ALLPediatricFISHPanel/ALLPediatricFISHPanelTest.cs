48887b079d8a146c7468c4fd456a7ce7fe197a07
WilliamCopland <william.copland@ypii.com>
2018-04-12 09:30:47 -0600

    Add ALLPediatricFISHPanel test

---
+++ YellowstonePathology/Business/Test/ALLPediatricFISHPanel/ALLPediatricFISHPanelTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowstonePathology.Business.Test.ALLPediatricFISHPanel
+{
+    public class ALLPediatricFISHPanelTest : YellowstonePathology.Business.PanelSet.Model.PanelSet
+    {
+        public ALLPediatricFISHPanelTest()
+        {
+            this.m_PanelSetId = 295;
+            this.m_PanelSetName = "ALL Pediatric FISH Panel";
+            this.m_CaseType = YellowstonePathology.Business.CaseType.FISH;
+            this.m_HasTechnicalComponent = true;
+            this.m_HasProfessionalComponent = true;
+            this.m_ResultDocumentSource = YellowstonePathology.Business.PanelSet.Model.ResultDocumentSourceEnum.PublishedDocument;
+            this.m_ReportNoLetter = new YellowstonePathology.Business.ReportNoLetterR();
+            this.m_Active = true;
+            this.m_IsBillable = true;
+            this.m_HasNoOrderTarget = false;
+            this.m_ExpectedDuration = TimeSpan.FromDays(10);
+
+            this.m_PanelSetOrderClassName = typeof(YellowstonePathology.Business.Test.PanelSetOrder).AssemblyQualifiedName;
+            this.m_WordDocumentClassName = typeof(YellowstonePathology.Business.Document.ReferenceLabReport).AssemblyQualifiedName;
+            this.m_AllowMultiplePerAccession = true;
+
+            this.m_TechnicalComponentFacility = new YellowstonePathology.Business.Facility.Model.NeogenomicsIrvine();
+            this.m_TechnicalComponentBillingFacility = new YellowstonePathology.Business.Facility.Model.YellowstonePathologyInstituteBillings();
+
+            this.m_ProfessionalComponentFacility = new YellowstonePathology.Business.Facility.Model.YellowstonePathologistBillings();
+            this.m_ProfessionalComponentBillingFacility = new YellowstonePathology.Business.Facility.Model.YellowstonePathologyInstituteBillings();
+
+            string taskDescription = "Collect paraffin block from Histology and send to Neo.";
+            this.m_TaskCollection.Add(new YellowstonePathology.Business.Task.Model.TaskFedexShipment(YellowstonePathology.Business.Task.Model.TaskAssignment.Transcription, taskDescription, new Facility.Model.NeogenomicsIrvine()));
+
+            this.m_UniversalServiceIdCollection.Add(new YellowstonePathology.Business.ClientOrder.Model.UniversalServiceDefinitions.UniversalServiceMOLEGEN());
+        }
+    }
+}
