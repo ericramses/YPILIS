6743fde07f53e054f40862026eef77b0c8a6210e
WilliamCopland <william.copland@ypii.com>
2018-06-20 09:32:15 -0600

    Create webServiceAccount classes, sqlServerDataReaderPropertyWriter

---
+++ YellowstonePathology/Business/Gateway/WebServiceGateway.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace YellowstonePathology.Business.Gateway
+{
+    public class WebServiceGateway
+    {
+        public static Business.WebService.WebServiceAccountCollection GetWebServiceAccountCollection()
+        {
+            WebService.WebServiceAccountCollection result = new WebService.WebServiceAccountCollection();
+
+           SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "Select * from tblWebServiceAccount order by UserName;";
+            cmd.CommandType = CommandType.Text;
+
+            using (SqlConnection cn = new SqlConnection(YellowstonePathology.Properties.Settings.Default.SqlServerConnectionString))
+            {
+                cn.Open();
+                cmd.Connection = cn;
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        WebService.WebServiceAccount webServiceAccount = new WebService.WebServiceAccount();
+                        YellowstonePathology.Business.Persistence.SqlServerDataReaderPropertyWriter sqlServerDataReaderPropertyWriter = new Persistence.SqlServerDataReaderPropertyWriter(webServiceAccount, dr);
+                        sqlServerDataReaderPropertyWriter.WriteProperties();
+                        result.Add(webServiceAccount);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ YellowstonePathology/Business/WebService/WebServiceAccountClient.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using YellowstonePathology.Business.Persistence;
+
+namespace YellowstonePathology.Business.WebService
+{
+    [PersistentClass("tblWebServiceAccountClient", "YPIDATA")]
+    public class WebServiceAccountClient : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int m_WebServiceAccountClientId;
+        private int m_WebServiceAccountId;
+        private int m_ClientId;
+        private string m_ObjectId;
+
+        public WebServiceAccountClient()
+        { }
+
+        public void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        [PersistentPrimaryKeyProperty(true)]
+        public int WebServiceAccountClientId
+        {
+            get { return this.m_WebServiceAccountClientId; }
+            set
+            {
+                if (this.m_WebServiceAccountClientId != value)
+                {
+                    this.m_WebServiceAccountClientId = value;
+                    this.NotifyPropertyChanged("WebServiceAccountClientId");
+                }
+            }
+        }
+
+        [PersistentProperty()]
+        public int WebServiceAccountId
+        {
+            get { return this.m_WebServiceAccountId; }
+            set
+            {
+                if (this.m_WebServiceAccountId != value)
+                {
+                    this.m_WebServiceAccountId = value;
+                    this.NotifyPropertyChanged("WebServiceAccountId");
+                }
+            }
+        }
+
+        [PersistentProperty()]
+        public int ClientId
+        {
+            get { return this.m_ClientId; }
+            set
+            {
+                if (this.m_ClientId != value)
+                {
+                    this.m_ClientId = value;
+                    this.NotifyPropertyChanged("ClientId");
+                }
+            }
+        }
+
+        [PersistentProperty()]
+        public string ObjectId
+        {
+            get { return this.m_ObjectId; }
+            set
+            {
+                if (this.m_ObjectId != value)
+                {
+                    this.m_ObjectId = value;
+                    this.NotifyPropertyChanged("ObjectId");
+                }
+            }
+        }
+    }
+}
