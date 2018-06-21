284a4ff07d0f8200aefc07fb4e6be5b5bd19a00b
WilliamCopland <william.copland@ypii.com>
2018-06-21 10:32:15 -0600

    Refactor test from stain switch into TestFactory

---
+++ YellowstonePathology/Business/Test.Model/TestFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowstonePathology.Business.Test.Model
+{
+    public class TestFactory
+    {
+
+        public static Test TestFromStain(Stain.Model.Stain stain)
+        {
+            Test result = null;
+            switch (stain.StainType)
+            {
+                case "IHC":
+                    {
+                        result = new ImmunoHistochemistryTest(stain);
+                        break;
+                    }
+                case "CytochemicalStain":
+                    {
+                        result = new CytochemicalTest(stain);
+                        break;
+                    }
+                case "CytochemicalForMicroorganisms":
+                    {
+                        result = new CytochemicalForMicroorganisms(stain);
+                        break;
+                    }
+                case "GradedStain":
+                    {
+                        result = new GradedTest(stain);
+                        break;
+                    }
+                case "DualStain":
+                    {
+                        result = new DualStain(stain);
+                        break;
+                    }
+            }
+            return result;
+
+        }
+}
+}
