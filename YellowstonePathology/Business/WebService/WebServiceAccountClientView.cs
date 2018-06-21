5ed42700c0423e65b358e7c72ea77108f7c5e2b5
WilliamCopland <william.copland@ypii.com>
2018-06-21 11:57:41 -0600

    add WebServiceAccountClientView and collection

---
+++ YellowstonePathology/Business/WebService/WebServiceAccountClientView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowstonePathology.Business.WebService
+{
+    public class WebServiceAccountClientView
+    {
+        private string m_ClientName;
+
+        private WebServiceAccountClient m_WebServiceAccountClient;
+
+        public WebServiceAccountClientView()
+        { }
+
+        public string ClientName
+        {
+            get { return this.m_ClientName; }
+            set { this.m_ClientName = value; }
+        }
+
+        public WebServiceAccountClient WebServiceAccountClient
+        {
+            get { return this.m_WebServiceAccountClient; }
+            set { this.m_WebServiceAccountClient = value; }
+        }
+    }
+}
