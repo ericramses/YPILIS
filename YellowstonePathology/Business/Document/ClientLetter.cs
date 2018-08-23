3b875fa2f5d528040afdd03a50cac81128a5deb8
sidharder <sideharder@gmail.com>
2018-08-23 15:58:13 -0600

    client letter changes

--- YellowstonePathology/Business/Document/ClientLetter.cs
+++ YellowstonePathology/Business/Document/ClientLetter.cs
@@ -15,14 +15,12 @@ namespace YellowstonePathology.Business.Document
 
 		public void Create(string patientName, YellowstonePathology.Business.Client.Model.Client client, string letterBody)
         {
-            string templateName = @"\\CFileServer\documents\ReportTemplates\XmlTemplates\ClientMissingInfoFax.xml";            
+            this.m_TemplateName = @"\\CFileServer\documents\ReportTemplates\XmlTemplates\ClientMissingInfoFax.xml";            
+            this.m_ReportXml = new XmlDocument();
+            this.m_NameSpaceManager = new XmlNamespaceManager(this.m_ReportXml.NameTable);
+            this.m_NameSpaceManager.AddNamespace("w", "http://schemas.microsoft.com/office/word/2003/wordml");            
 
-            XmlDocument xmlDocument = new XmlDocument();
-            XmlNamespaceManager nameSpaceManager = new XmlNamespaceManager(m_ReportXml.NameTable);
-            nameSpaceManager.AddNamespace("w", "http://schemas.microsoft.com/office/word/2003/wordml");            
-
-            xmlDocument.Load(templateName);
-            this.m_ReportXml = xmlDocument;
+            this.m_ReportXml.Load(m_TemplateName);            
 
             this.ReplaceText("client_name", client.ClientName);
             this.ReplaceText("fax_number", YellowstonePathology.Business.Domain.PhoneNumber.Format(client.Fax));
@@ -32,7 +30,7 @@ namespace YellowstonePathology.Business.Document
             this.SetXMLNodeParagraphData("letter_body", letterBody);
 
             string xmlDocumentFileName = YellowstonePathology.Properties.Settings.Default.ClientMissingInformationLetterFileName.ToUpper().Replace("DOC", "XML"); 
-            xmlDocument.Save(xmlDocumentFileName);
+            this.m_ReportXml.Save(xmlDocumentFileName);
 
             //YellowstonePathology.Business.Document.CaseDocument.SaveXMLAsDocFromFileName(xmlDocumentFileName);
             Business.Helper.FileConversionHelper.ConvertXMLToDoc(xmlDocumentFileName, xmlDocumentFileName.Replace(".xml", ".doc"));
