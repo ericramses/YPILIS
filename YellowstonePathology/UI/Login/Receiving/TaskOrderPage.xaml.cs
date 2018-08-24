93bfe637d5289d28a108be8a0679194bfff9f1e4
WilliamCopland <william.copland@ypii.com>
2018-08-24 09:09:12 -0600

    Masked textBox, rm long distance, unused classes

--- YellowstonePathology/UI/Login/Receiving/TaskOrderPage.xaml.cs
+++ YellowstonePathology/UI/Login/Receiving/TaskOrderPage.xaml.cs
@@ -473,7 +473,6 @@ namespace YellowstonePathology.UI.Login.Receiving
             Business.Client.Model.Client client = Business.Gateway.PhysicianClientGateway.GetClientByClientId(this.m_AccessionOrder.ClientId);
             YellowstonePathology.Business.Task.Model.TaskOrderDetailFax taskOrderDetail = new Business.Task.Model.TaskOrderDetailFax(taskOrderDetailId, this.m_TaskOrder.TaskOrderId, objectId, task, this.m_AccessionOrder.ClientId);
             taskOrderDetail.FaxNumber = client.Fax;
-            if (client.LongDistance == true) taskOrderDetail.FaxNumber = "1" + taskOrderDetail.FaxNumber;
             this.m_TaskOrder.TaskOrderDetailCollection.Add(taskOrderDetail);
         }
     }
