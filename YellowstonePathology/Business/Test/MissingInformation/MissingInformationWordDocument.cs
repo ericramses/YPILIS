809cf4835c6ff5ff7fc09bdc83985a619ca91dd3
sidharder <sideharder@gmail.com>
2018-08-27 12:38:55 -0600

    missing information

--- YellowstonePathology/Business/Test/MissingInformation/MissingInformationWordDocument.cs
+++ YellowstonePathology/Business/Test/MissingInformation/MissingInformationWordDocument.cs
@@ -8,10 +8,12 @@ namespace YellowstonePathology.Business.Test.MissingInformation
 {
 	public class MissingInformationWordDocument : YellowstonePathology.Business.Document.CaseReportV2
 	{
-        public MissingInformationWordDocument(Business.Test.AccessionOrder accessionOrder, Business.Test.PanelSetOrder panelSetOrder, YellowstonePathology.Business.Document.ReportSaveModeEnum reportSaveMode) 
+        private string m_LetterBody;
+
+        public MissingInformationWordDocument(Business.Test.AccessionOrder accessionOrder, Business.Test.PanelSetOrder panelSetOrder, YellowstonePathology.Business.Document.ReportSaveModeEnum reportSaveMode, string letterBody) 
             : base(accessionOrder, panelSetOrder, reportSaveMode)
         {
-
+            this.m_LetterBody = letterBody;
         }
 
         public override void Render()
@@ -20,11 +22,12 @@ namespace YellowstonePathology.Business.Test.MissingInformation
             base.OpenTemplate();
 			this.SetDemographicsV2();
 
+            Business.Client.Model.Client client = Business.Gateway.PhysicianClientGateway.GetClientByClientId(this.m_AccessionOrder.ClientId);
             this.ReplaceText("client_name", this.m_AccessionOrder.ClientName);
-            //this.ReplaceText("fax_number", YellowstonePathology.Business.Domain.PhoneNumber.Format(client.Fax));
+            this.ReplaceText("fax_number", YellowstonePathology.Business.Domain.PhoneNumber.Format(client.Fax));
             this.ReplaceText("current_date", DateTime.Today.ToShortDateString() + " " + DateTime.Now.ToShortTimeString());
             this.ReplaceText("patient_name", this.m_AccessionOrder.PatientDisplayName);
-            //this.SetXMLNodeParagraphData("letter_body", letterBody);
+            this.SetXMLNodeParagraphData("letter_body", this.m_LetterBody);
 
             YellowstonePathology.Business.OrderIdParser orderIdParser = new YellowstonePathology.Business.OrderIdParser(this.m_PanelSetOrder.ReportNo);
             this.m_SaveFileName = Business.Document.CaseDocument.GetCaseFileNameXMLPreAuth(orderIdParser);
