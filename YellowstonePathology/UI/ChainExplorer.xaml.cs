a3d515aeb3e487e0a9dde231c60fe41693d588c3
sidharder <sideharder@gmail.com>
2018-10-03 14:45:49 -0600

    chain

--- YellowstonePathology/UI/ChainExplorer.xaml.cs
+++ YellowstonePathology/UI/ChainExplorer.xaml.cs
@@ -16,8 +16,11 @@ namespace YellowstonePathology.UI
 {  
     public partial class ChainExplorer : Window
     {
+        Business.GethAPI m_GethAPI;
+
         public ChainExplorer()
         {
+            this.m_GethAPI = new Business.GethAPI();
             InitializeComponent();
         }
 
@@ -31,22 +34,27 @@ namespace YellowstonePathology.UI
             //gethAPI.GetTransactionReceipt("0x5cb44be5bc15fcb45210f4bb5b0e41adb607548c0b755d6f5cb22ebba985c2fc");
         }
 
-        private void GetAverageBlockTime()
+        private void GetFirstBlockForDate(DateTime workDate)
         {
-            Business.GethAPI gethAPI = new Business.GethAPI();
-            Business.EthBlock latestBlock = gethAPI.GetLatestBlockNumber();
+            double blockTime = this.GetAverageBlockTime();
+            Business.EthBlock latestBlock = this.m_GethAPI.GetLatestBlock();
+                        
+        }
 
+        private double GetAverageBlockTime()
+        {            
+            Business.EthBlock latestBlock = this.m_GethAPI.GetLatestBlock();
             List<double> blockTimes = new List<double>();
 
-            Business.EthBlock lastBlock = gethAPI.GetBlockByNumber(latestBlock.Number - 100);
+            Business.EthBlock lastBlock = this.m_GethAPI.GetBlockByNumber(latestBlock.Number - 100);
             for (int i=latestBlock.Number - 99; i<latestBlock.Number; i++)
             {
-                Business.EthBlock thisBlock = gethAPI.GetBlockByNumber(i);
+                Business.EthBlock thisBlock = this.m_GethAPI.GetBlockByNumber(i);
                 blockTimes.Add(thisBlock.TimeStamp.Subtract(lastBlock.TimeStamp).TotalSeconds);
                 lastBlock = thisBlock;
             }
 
-            double average = blockTimes.Average();            
+            return blockTimes.Average();            
         }
     }
 }
