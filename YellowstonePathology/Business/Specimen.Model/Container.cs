bb5e2ff16c83d7d2082ab1ef3f83ce30633c63b9
sidharder <sideharder@gmail.com>
2018-10-15 08:37:50 -0600

    container

---
+++ YellowstonePathology/Business/Specimen.Model/Container.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowstonePathology.Business.Specimen.Model
+{
+    public class Container
+    {
+        string m_ContainerId;
+        Nullable<DateTime> m_Shipped;
+        Nullable<DateTime> m_Received;        
+
+        public Container(string ethResult)
+        {
+            ethResult = ethResult.Remove(0, 2);
+
+            string hexShipped = ethResult.Substring(64, 64);
+            int seconds = int.Parse(hexShipped, System.Globalization.NumberStyles.HexNumber);
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            this.m_Shipped = dtDateTime.AddSeconds(seconds).ToLocalTime();
+
+            string hexContainerId = ethResult.Substring(128);
+            string[] split = this.GetSplit(hexContainerId);
+            this.m_ContainerId = FromHex(split).Substring(64, 40);
+        }            
+        
+        private string FromHex(string [] split)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (string hex in split)
+            {                
+                int value = Convert.ToInt32(hex, 16);             
+                result.Append(Char.ConvertFromUtf32(value));                
+            }            
+            return result.ToString().Trim();
+        }
+
+        private string [] GetSplit(string hexString)
+        {
+            StringBuilder spaced = new StringBuilder();
+            for(int i=0; i<hexString.Length; i++)
+            {
+                if(i != 0 && i % 2 == 0)
+                {
+                    spaced.Append(' ');
+                }
+                spaced.Append(hexString[i]);
+            }            
+            return spaced.ToString().Split(' ');
+        }    
+
+        public string ContainerId
+        {
+            get { return this.m_ContainerId; }
+            set { this.m_ContainerId = value; }
+        }
+
+        public Nullable<DateTime> Shipped
+        {
+            get { return this.m_Shipped; }
+            set { this.m_Shipped = value; }
+        }
+
+        public Nullable<DateTime> Received
+        {
+            get { return this.m_Received; }
+            set { this.m_Received = value; }
+        }        
+    }
+}
---
+++ YellowstonePathology/UI/Billing/SVHMRNMatcher.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowstonePathology.UI.Billing
+{
+    public class SVHMRNMatcher
+    {
+        private Business.Test.AccessionOrder m_AccessionOrder;
+        private string m_ANumber;
+        private string m_VNumber;
+        private string m_VNumberAccount;
+        private bool m_MatchFound;
+
+        public SVHMRNMatcher(Business.Test.AccessionOrder accessionOrder)
+        {
+            this.m_AccessionOrder = accessionOrder;
+        }
+
+        public void Match()
+        {
+            if(string.IsNullOrEmpty(this.m_AccessionOrder.SvhMedicalRecord) == false)
+            {
+                if (this.m_AccessionOrder.SvhMedicalRecord.StartsWith("A") == true)
+                {
+                    this.m_ANumber = this.m_AccessionOrder.SvhMedicalRecord;
+                    Business.ClientOrder.Model.ClientOrderCollection clientOrders = Business.Gateway.ClientOrderGateway.GetClientOrdersByANumber(this.m_AccessionOrder.SvhMedicalRecord);
+                    if(clientOrders.Count > 0)
+                    {
+                        this.m_VNumber = clientOrders[0].SvhMedicalRecord;
+                        this.m_VNumberAccount = clientOrders[0].SvhAccountNo;
+                        this.m_MatchFound = true;
+                    }
+                    else
+                    {
+                        this.m_VNumber = null;
+                        this.m_MatchFound = false;
+                    }
+                }
+                else
+                {
+                    this.m_VNumber = this.m_AccessionOrder.SvhMedicalRecord;
+                    this.m_ANumber = null;
+                    this.m_MatchFound = false;
+                }
+            }
+            else
+            {
+                this.m_ANumber = null;
+                this.m_MatchFound = false;
+            }            
+        }        
+
+        public string ANumber
+        {
+            get { return this.m_ANumber; }
+            set { this.m_ANumber = value; }
+        }
+
+        public string VNumber
+        {
+            get { return this.m_VNumber; }
+            set { this.m_VNumber = value; }
+        }
+
+        public string VNumberAccount
+        {
+            get { return this.m_VNumberAccount; }
+            set { this.m_VNumberAccount = value; }
+        }
+
+        public bool MatchFound
+        {
+            get { return this.m_MatchFound; }
+            set { this.m_MatchFound = value; }
+        }
+    }
+}
