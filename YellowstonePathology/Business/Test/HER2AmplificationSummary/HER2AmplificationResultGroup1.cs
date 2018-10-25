dbb0f128f0e3dc7265c36c7b18caaa5f95f6ccfa
WilliamCopland <william.copland@ypii.com>
2018-10-25 10:00:36 -0600

    Refactor Result and Group2

--- YellowstonePathology/Business/Test/HER2AmplificationSummary/HER2AmplificationResultGroup1.cs
+++ YellowstonePathology/Business/Test/HER2AmplificationSummary/HER2AmplificationResultGroup1.cs
@@ -15,7 +15,7 @@ namespace YellowstonePathology.Business.Test.HER2AmplificationSummary
 
         public override void IsAMatch( HER2AmplificationResultMatch her2AmplificationResultMatch)
         {
-            if (this.AverageHER2CopyNo >= 4.0 && this.HER2CEP17Ratio >= 2)
+            if (this.HER2CEP17Ratio >= 2 && this.AverageHER2CopyNo >= 4.0)
             {
                 her2AmplificationResultMatch.IsAMatch = true;
                 her2AmplificationResultMatch.Result = HER2AmplificationResultEnum.Positive;
