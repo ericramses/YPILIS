330df1260b17bcdc3551a35a87d18eb5012a5ab1
sidharder <sideharder@gmail.com>
2018-11-02 15:44:08 -0600

    cassette printer

--- YellowstonePathology/Business/Label.Model/CassettePrinterGrossTech.cs
+++ YellowstonePathology/Business/Label.Model/CassettePrinterGrossTech.cs
@@ -12,9 +12,9 @@ namespace YellowstonePathology.Business.Label.Model
 
         public CassettePrinterGrossTech() : base("Gross Tech", @"\\10.1.1.84\Jobs")
         {
-            this.Carousel.Columns.Add(new CarouselColumn("Yellow", 1, 101, "Yellow"));
-            this.Carousel.Columns.Add(new CarouselColumn("White", 2, 102, "White"));
-            this.Carousel.Columns.Add(new CarouselColumn("Orange", 3, 103, "#FFFFC175"));
+            this.Carousel.Columns.Add(new CarouselColumn("White", 1, 101, "White"));
+            this.Carousel.Columns.Add(new CarouselColumn("Orange", 2, 102, "#FFFFC175"));
+            this.Carousel.Columns.Add(new CarouselColumn("Yellow", 3, 103, "Yellow"));
             this.Carousel.Columns.Add(new CarouselColumn("Lilac", 4, 104, "#b666d2"));            
         }
 
