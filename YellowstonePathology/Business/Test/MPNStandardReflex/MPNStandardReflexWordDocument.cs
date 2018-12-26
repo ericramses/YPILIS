eae3dff29242dadf98bf5e73b649a6f149af3e11
WilliamCopland <william.copland@ypii.com>
2018-12-26 10:33:58 -0700

    Use correct word doc version for MPNStandardReflex, fix dump in LSEResult

--- YellowstonePathology/Business/Test/MPNStandardReflex/MPNStandardReflexWordDocument.cs
+++ YellowstonePathology/Business/Test/MPNStandardReflex/MPNStandardReflexWordDocument.cs
@@ -21,7 +21,7 @@ namespace YellowstonePathology.Business.Test.MPNStandardReflex
 			YellowstonePathology.Business.Test.MPNStandardReflex.PanelSetOrderMPNStandardReflex panelSetOrderMPNStandardReflex = (YellowstonePathology.Business.Test.MPNStandardReflex.PanelSetOrderMPNStandardReflex)this.m_AccessionOrder.PanelSetOrderCollection.GetPanelSetOrder(mpnStandardReflex.PanelSetId);
             YellowstonePathology.Business.Specimen.Model.SpecimenOrder specimenOrder = this.m_AccessionOrder.SpecimenOrderCollection.GetSpecimenOrder(this.m_PanelSetOrder.OrderedOn, this.m_PanelSetOrder.OrderedOnId);
 
-            this.m_TemplateName = @"\\CFileServer\Documents\ReportTemplates\XmlTemplates\MPNStandardReflex.1.xml";
+            this.m_TemplateName = @"\\CFileServer\Documents\ReportTemplates\XmlTemplates\MPNStandardReflex.2.xml";
 			base.OpenTemplate();            
 
 			this.SetDemographicsV2();
