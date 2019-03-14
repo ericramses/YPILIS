7b83f58dfedb71bb73b9ffd4ba75d4a7cba13df6
WilliamCopland <william.copland@ypii.com>
2019-03-14 10:01:46 -0600

    Retire Flow version of Stem Cell Enum

---
+++ YellowstonePathology/Business/Test/StemCellEnumeration/StemCellEnumerationTestRetired.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowstonePathology.Business.Test.StemCellEnumeration
+{
+    public class StemCellEnumerationTestRetired : YellowstonePathology.Business.PanelSet.Model.FlowCytometry.PanelSetFlowCytometry
+    {
+        public StemCellEnumerationTestRetired()
+        {
+            this.m_PanelSetId = 24;
+            this.m_PanelSetName = "Stem Cell Enumeration - Retired";
+            //this.m_TestId = 12;
+            this.m_AllowMultiplePerAccession = true;
+            this.m_ExpectedDuration = new TimeSpan(24, 0, 0);
+            this.m_WordDocumentClassName = typeof(YellowstonePathology.Business.Test.StemCellEnumeration.StemCellEnumerationWordDocument).AssemblyQualifiedName;
+
+            string taskDescription = "Perform stem cell enum. testing.";
+            this.m_TaskCollection.Add(new YellowstonePathology.Business.Task.Model.Task(YellowstonePathology.Business.Task.Model.TaskAssignment.Flow, taskDescription));
+
+            this.m_HasProfessionalComponent = false;
+            this.m_ProfessionalComponentFacility = null;
+
+            YellowstonePathology.Business.Billing.Model.PanelSetCptCode panelSetCptCode1 = new Business.Billing.Model.PanelSetCptCode(Store.AppDataStore.Instance.CPTCodeCollection.GetClone("86367", null), 1);
+            this.m_PanelSetCptCodeCollection.Add(panelSetCptCode1);
+        }
+    }
+}
