749c30471055e596a9cba5146adbbee4f21a2ee0
WilliamCopland <william.copland@ypii.com>
2019-03-15 09:27:11 -0600

    Add StandingOrderView and collection wiht dlg to show them

---
+++ YellowstonePathology/Business/Client.Model/StandingOrderViewCollection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace YellowstonePathology.Business.Client.Model
+{
+    public class StandingOrderViewCollection : ObservableCollection<StandingOrderView>
+    {
+        public StandingOrderViewCollection()
+        {
+            StandingOrderCollection standingOrderCollection = StandingOrderCollection.GetAll();
+            foreach(StandingOrder standingOrder in standingOrderCollection)
+            {
+                StandingOrderView view = new Model.StandingOrderView(standingOrder);
+                this.Add(view);
+            }
+        }
+    }
+}
