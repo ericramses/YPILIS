2b16f86cda9dc5943ca240667b71cbd2aceb0a4e
sidharder <sideharder@gmail.com>
2019-03-19 10:44:19 -0600

    cell cycle

---
+++ YellowstonePathology/Business/Test/DNACellCycleAnalysis/DNACellCycleAnalysisTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YellowstonePathology.Business.Test.DNACellCycleAnalysis
+{
+	public class DNACellCycleAnalysisTest : YellowstonePathology.Business.PanelSet.Model.FlowCytometry.PanelSetFlowCytometry
+    {
+        public DNACellCycleAnalysisTest()
+        {
+            this.m_PanelSetId = 29;
+            this.m_PanelSetName = "DNA Content and Cell Cycle Analysis";            
+			this.m_AllowMultiplePerAccession = true;
+            this.m_ExpectedDuration = new TimeSpan(24, 0, 0);
+
+            /*
+            this.m_EpicDistributionIsImplemented = true;
+            this.m_WordDocumentClassName = typeof(YellowstonePathology.Business.Test.ThombocytopeniaProfile.ThombocytopeniaProfileWordDocument).AssemblyQualifiedName;
+
+            string taskDescription = "Perform thrombocytopenia profile testing.";
+			this.m_TaskCollection.Add(new YellowstonePathology.Business.Task.Model.Task(YellowstonePathology.Business.Task.Model.TaskAssignment.Flow, taskDescription));
+
+            this.m_HasProfessionalComponent = false;
+            this.m_ProfessionalComponentFacility = null;            
+            */
+		}
+    }
+}
