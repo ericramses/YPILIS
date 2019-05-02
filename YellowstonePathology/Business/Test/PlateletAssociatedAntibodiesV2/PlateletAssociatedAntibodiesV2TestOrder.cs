be6487bdc3541e61e98e02ead67889e0754c41e8
WilliamCopland <william.copland@ypii.com>
2019-05-02 11:14:33 -0600

    Add PlateletAssociatedAntibodiesV2

---
+++ YellowstonePathology/Business/Test/PlateletAssociatedAntibodiesV2/PlateletAssociatedAntibodiesV2TestOrder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowstonePathology.Business.Test.PlateletAssociatedAntibodiesV2
+{
+    class PlateletAssociatedAntibodiesV2TestOrder
+    {
+    }
+}
