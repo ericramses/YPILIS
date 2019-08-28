dbaf0566a75699551508b843f05371ad689ac9ac
WilliamCopland <william.copland@ypii.com>
2019-08-28 10:26:21 -0600

    use refactored add Beaker obx element

--- YellowstonePathology/Business/Test/NGCT/NGCTEPICBeakerObxView.cs
+++ YellowstonePathology/Business/Test/NGCT/NGCTEPICBeakerObxView.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Xml.Linq;
 
 namespace YellowstonePathology.Business.Test.NGCT
@@ -14,27 +15,46 @@ namespace YellowstonePathology.Business.Test.NGCT
         public override void ToXml(XElement document)
         {
             NGCTTestOrder testOrder = (NGCTTestOrder)this.m_AccessionOrder.PanelSetOrderCollection.GetPanelSetOrder(this.m_ReportNo);
+            YellowstonePathology.Business.Amendment.Model.AmendmentCollection amendmentCollection = this.m_AccessionOrder.AmendmentCollection.GetAmendmentsForReport(this.m_ReportNo);
 
-            this.AddHeader(document, testOrder, "Chlamydia Gonorrhea Screening");
+            this.AddNextObxElementBeaker("Report No: ", this.m_ReportNo, document, "F");
 
-            this.AddNextObxElementBeaker("CHLAMYDIATRACHOMATISRESULT", "Chlamydia trachomatis result: " + testOrder.ChlamydiaTrachomatisResult, document, "F");
-            this.AddNextObxElementBeaker("CHLAMYDIATRACHOMATISRESULTREFERENCE", "Reference: Negative", document, "F");
+            this.AddNextObxElementBeaker("Chlamydia trachomatis result: ", testOrder.ChlamydiaTrachomatisResult, document, "F");
+            this.AddNextObxElementBeaker("Reference: ", "Negative", document, "F");
 
-            this.AddNextObxElementBeaker("NEISSERIAGONORROEAERESULT", "Neisseria gonorrhoeae result: " + testOrder.NeisseriaGonorrhoeaeResult, document, "F");
-            this.AddNextObxElementBeaker("NEISSERIAGONORROEARESULTREFERENCE", "Reference: Negative", document, "F");
+            this.AddNextObxElementBeaker("Neisseria gonorrhoeae result: ", testOrder.NeisseriaGonorrhoeaeResult, document, "F");
+            this.AddNextObxElementBeaker("Reference: ", "Negative", document, "F");
 
-            this.AddAmendments(document);
+            if (amendmentCollection.Count != 0)
+            {
+                StringBuilder amendments = new StringBuilder();
+                foreach (YellowstonePathology.Business.Amendment.Model.Amendment amendment in amendmentCollection)
+                {
+                    if (amendment.Final == true)
+                    {
+                        amendments.AppendLine(amendment.AmendmentType + ": " + amendment.AmendmentDate.Value.ToString("MM/dd/yyyy"));
+                        amendments.AppendLine(amendment.Text);
+                        if (amendment.RequirePathologistSignature == true)
+                        {
+                            amendments.AppendLine("Signature: " + amendment.PathologistSignature);
+                            amendments.AppendLine("E-signed " + amendment.FinalTime.Value.ToString("MM/dd/yyyy HH:mm"));
+                        }
+                    }
+                }
+                amendments.AppendLine();
+                this.AddNextObxElementBeaker("Amendments", amendments.ToString(), document, "F");
+            }
 
-            this.AddNextObxElementBeaker("SPECIMENDESCRIPTION", "Specimen: Thin Prep Fluid", document, "F");
+            this.AddNextObxElementBeaker("Specimen: ", "Thin Prep Fluid", document, "F");
 
-            this.AddNextObxElementBeaker("METHOD", "Method: " + testOrder.Method, document, "F");
+            this.AddNextObxElementBeaker("Method: ", testOrder.Method, document, "F");
 
-            this.AddNextObxElementBeaker("REFERENCES", "References: " + testOrder.ReportReferences, document, "F");
+            this.AddNextObxElementBeaker("References: ", testOrder.ReportReferences, document, "F");
 
-            this.AddNextObxElementBeaker("TESTINFORMATION", "Test Information: " + testOrder.TestInformation, document, "F");
+            this.AddNextObxElementBeaker("Test Information: ", testOrder.TestInformation, document, "F");
 
             string locationPerformed = testOrder.GetLocationPerformedComment();
-            this.AddNextObxElementBeaker("LOCATIONPERFORMED", "Location Performed: " + locationPerformed, document, "F");
+            this.AddNextObxElementBeaker("Location Performed: ", locationPerformed, document, "F");
         }
     }
 }
